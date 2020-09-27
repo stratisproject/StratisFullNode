6160565ac2b940cb626a77c441799a806cee441f
quantumagi <someguy.fromafrica@gmail.com>
2020-09-27 19:11:01 +1000

    Ensure normal balances can't be affected by ColdStaking scripts

---
+++ src/Stratis.Bitcoin.Features.Wallet/AccountRestrictedKeyId.cs
@@ -0,0 +1,17 @@
+﻿using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.Wallet
+{
+    public class AccountRestrictedKeyId : KeyId
+    {
+        public int AccountId { get; private set; }
+
+        public AccountRestrictedKeyId(byte[] keyBytes, int accountId) : base(keyBytes)
+        {
+            this.AccountId = accountId;
+        }
+        public AccountRestrictedKeyId(KeyId keyId, int accountId) : this(keyId.ToBytes(), accountId)
+        {
+        }
+    }
+}
