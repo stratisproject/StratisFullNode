2c057135b1737efe33dddfd822f504323da05d36
quantumagi <someguy.fromafrica@gmail.com>
2020-09-30 14:22:26 +1000

    Change PayToFederationTemplate.ExtractScriptPubKeyParameters return type

--- src/NBitcoin/BuilderExtensions/P2FederationBuilderExtension.cs
+++ src/NBitcoin/BuilderExtensions/P2FederationBuilderExtension.cs
@@ -8,7 +8,7 @@ namespace NBitcoin.BuilderExtensions
     {
         public override bool CanCombineScriptSig(Network network, Script scriptPubKey, Script a, Script b)
         {
-            return PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey) != null;
+            return PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey, network) != null;
         }
 
         public override bool CanDeduceScriptPubKey(Network network, Script scriptSig)
@@ -18,18 +18,17 @@ namespace NBitcoin.BuilderExtensions
 
         public override bool CanEstimateScriptSigSize(Network network, Script scriptPubKey)
         {
-            return PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey) != null;
+            return PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey, network) != null;
         }
 
         public override bool CanGenerateScriptSig(Network network, Script scriptPubKey)
         {
-            return PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey) != null;
+            return PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey, network) != null;
         }
 
         public override Script CombineScriptSig(Network network, Script scriptPubKey, Script a, Script b)
         {
-            byte[] federationId = PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey);
-            (PubKey[] pubKeys, int signatureCount) = network.Federation.GetFederationDetails(federationId);
+            PayToMultiSigTemplateParameters para = PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey, network);
 
             // Combine all the signatures we've got:
             TransactionSignature[] aSigs = PayToFederationTemplate.Instance.ExtractScriptSigParameters(network, a);
@@ -39,8 +38,8 @@ namespace NBitcoin.BuilderExtensions
             if (bSigs == null)
                 return a;
             int sigCount = 0;
-            var sigs = new TransactionSignature[pubKeys.Length];
-            for (int i = 0; i < pubKeys.Length; i++)
+            var sigs = new TransactionSignature[para.PubKeys.Length];
+            for (int i = 0; i < para.PubKeys.Length; i++)
             {
                 TransactionSignature aSig = i < aSigs.Length ? aSigs[i] : null;
                 TransactionSignature bSig = i < bSigs.Length ? bSigs[i] : null;
@@ -50,10 +49,10 @@ namespace NBitcoin.BuilderExtensions
                     sigs[i] = sig;
                     sigCount++;
                 }
-                if (sigCount == signatureCount)
+                if (sigCount == para.SignatureCount)
                     break;
             }
-            if (sigCount == signatureCount)
+            if (sigCount == para.SignatureCount)
                 sigs = sigs.Where(s => s != null && s != TransactionSignature.Empty).ToArray();
             return PayToFederationTemplate.Instance.GenerateScriptSig(sigs);
         }
@@ -65,25 +64,23 @@ namespace NBitcoin.BuilderExtensions
 
         public override int EstimateScriptSigSize(Network network, Script scriptPubKey)
         {
-            byte[] federationId = PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey);
-            (PubKey[] pubKeys, int sigsReq) = network.Federation.GetFederationDetails(federationId);
-            return PayToFederationTemplate.Instance.GenerateScriptSig(Enumerable.Range(0, sigsReq).Select(o => DummySignature).ToArray()).Length;
+            PayToMultiSigTemplateParameters para = PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey, network);
+            return PayToFederationTemplate.Instance.GenerateScriptSig(Enumerable.Range(0, para.SignatureCount).Select(o => DummySignature).ToArray()).Length;
         }
 
         public override Script GenerateScriptSig(Network network, Script scriptPubKey, IKeyRepository keyRepo, ISigner signer)
         {
-            byte[] federationId = PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey);
-            (PubKey[] pubKeys, int sigsReq) = network.Federation.GetFederationDetails(federationId);
-            var signatures = new TransactionSignature[pubKeys.Length];
+            PayToMultiSigTemplateParameters para = PayToFederationTemplate.Instance.ExtractScriptPubKeyParameters(scriptPubKey, network);
+            var signatures = new TransactionSignature[para.PubKeys.Length];
             Key[] keys =
-                pubKeys
+                para.PubKeys
                 .Select(p => keyRepo.FindKey(p.ScriptPubKey))
                 .ToArray();
 
             int sigCount = 0;
             for (int i = 0; i < keys.Length; i++)
             {
-                if (sigCount == sigsReq)
+                if (sigCount == para.SignatureCount)
                     break;
                 if (keys[i] != null)
                 {
@@ -94,7 +91,7 @@ namespace NBitcoin.BuilderExtensions
             }
 
             IEnumerable<TransactionSignature> sigs = signatures;
-            if (sigCount == sigsReq)
+            if (sigCount == para.SignatureCount)
             {
                 sigs = sigs.Where(s => s != TransactionSignature.Empty && s != null);
             }
