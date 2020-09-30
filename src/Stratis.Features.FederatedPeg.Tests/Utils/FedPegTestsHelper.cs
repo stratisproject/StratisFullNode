496bfd3dc5a27c81850160d11bd332c5ae8e4188
quantumagi <someguy.fromafrica@gmail.com>
2020-09-30 16:59:37 +1000

    Update InitializationTakesForeverIfCounterNodeIsOfflineAsync

--- src/Stratis.Features.FederatedPeg.Tests/Utils/FedPegTestsHelper.cs
+++ src/Stratis.Features.FederatedPeg.Tests/Utils/FedPegTestsHelper.cs
@@ -1,4 +1,5 @@
-﻿using NBitcoin;
+﻿using System.Linq;
+using NBitcoin;
 using NBitcoin.Protocol;
 using Stratis.Bitcoin.Configuration;
 
@@ -8,9 +9,9 @@ namespace Stratis.Features.FederatedPeg.Tests.Utils
     {
         public static FederatedPegSettings CreateSettings(Network network, out NodeSettings nodeSettings)
         {
-            string redeemScript = "2 02fad5f3c4fdf4c22e8be4cfda47882fff89aaa0a48c1ccad7fa80dc5fee9ccec3 02503f03243d41c141172465caca2f5cef7524f149e965483be7ce4e44107d7d35 03be943c3a31359cd8e67bedb7122a0898d2c204cf2d0119e923ded58c429ef97c 3 OP_CHECKMULTISIG";
+            string redeemScript = PayToFederationTemplate.Instance.GenerateScriptPubKey(network.Federation.Id).ToString();
             string federationIps = "127.0.0.1:36201,127.0.0.1:36202,127.0.0.1:36203";
-            string multisigPubKey = "03be943c3a31359cd8e67bedb7122a0898d2c204cf2d0119e923ded58c429ef97c";
+            string multisigPubKey = network.Federation.GetFederationDetails(network.Federation.Id).pubKeys.TakeLast(1).First().ToHex();
             string[] args = new[] { "-sidechain", "-regtest", $"-federationips={federationIps}", $"-redeemscript={redeemScript}", $"-publickey={multisigPubKey}", "-mincoinmaturity=1", "-mindepositconfirmations=1" };
             nodeSettings = new NodeSettings(network, ProtocolVersion.ALT_PROTOCOL_VERSION, args: args);
 
