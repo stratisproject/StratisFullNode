17ebd0816a909a30eb23c8d260da73e8461e65c1
Kevin Loubser <zeptin@gmail.com>
2020-10-23 07:46:31 +0200

    Get last transaction

---
+++ src/Stratis.Bitcoin.Features.BlockStore/Models/LastBalanceDecreaseTransactionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Stratis.Bitcoin.Features.BlockStore.Models
+{
+    public class LastBalanceDecreaseTransactionModel
+    {
+        public string TransactionHex { get; set; }
+
+        public int BlockHeight { get; set; }
+
+        //public long DecreasedBy { get; set; }
+    }
+}
