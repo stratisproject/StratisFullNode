9ffee36001ec566becd4b6be289b4d52067f5967
Francois de la Rouviere <fassadlr@gmail.com>
2020-11-12 14:46:34 +0000

    Update Program.cs (#167)

--- src/Stratis.StraxD/Program.cs
+++ src/Stratis.StraxD/Program.cs
@@ -1,13 +1,17 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
+using NBitcoin;
 using NBitcoin.Protocol;
 using Stratis.Bitcoin;
 using Stratis.Bitcoin.Builder;
 using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Configuration.Logging;
 using Stratis.Bitcoin.Features.Api;
 using Stratis.Bitcoin.Features.BlockStore;
 using Stratis.Bitcoin.Features.ColdStaking;
 using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.Consensus.ProvenBlockHeaders;
 using Stratis.Bitcoin.Features.MemoryPool;
 using Stratis.Bitcoin.Features.Miner;
 using Stratis.Bitcoin.Features.RPC;
@@ -31,6 +35,8 @@ namespace Stratis.StraxD
                 {
                     MinProtocolVersion = ProtocolVersion.PROVEN_HEADER_VERSION
                 };
+
+                await CheckLegacyGenesisHashAsync(nodeSettings);
 
                 IFullNodeBuilder nodeBuilder = new FullNodeBuilder()
                     .UseNodeSettings(nodeSettings)
@@ -76,6 +82,41 @@ namespace Stratis.StraxD
             catch (Exception ex)
             {
                 Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex);
+            }
+        }
+
+        private static async Task CheckLegacyGenesisHashAsync(NodeSettings nodeSettings)
+        {
+            var store = new ProvenBlockHeaderRepository(nodeSettings.Network, nodeSettings.DataFolder, ExtendedLoggerFactory.Create(), new DBreezeSerializer(nodeSettings.Network.Consensus.ConsensusFactory));
+            await store.InitializeAsync();
+            store.Dispose();
+
+            if (store.TipHashHeight.Hash == new uint256("00000921702bd55eb8c4318a8dbcfca29b9d340b1856c6af0b8962a3a0e12fff") && store.TipHashHeight.Height == 0)
+            {
+                if (Directory.Exists(Path.Combine(nodeSettings.DataDir, "blocks")))
+                    DeleteFolderContents(Path.Combine(nodeSettings.DataDir, "blocks"));
+
+                if (Directory.Exists(Path.Combine(nodeSettings.DataDir, "chain")))
+                    DeleteFolderContents(Path.Combine(nodeSettings.DataDir, "chain"));
+
+                if (Directory.Exists(Path.Combine(nodeSettings.DataDir, "coindb")))
+                    DeleteFolderContents(Path.Combine(nodeSettings.DataDir, "coindb"));
+
+                if (Directory.Exists(Path.Combine(nodeSettings.DataDir, "common")))
+                    DeleteFolderContents(Path.Combine(nodeSettings.DataDir, "common"));
+
+                if (Directory.Exists(Path.Combine(nodeSettings.DataDir, "provenheaders")))
+                    DeleteFolderContents(Path.Combine(nodeSettings.DataDir, "provenheaders"));
+
+                File.Delete(Path.Combine(nodeSettings.DataDir, "peers.json"));
+            }
+        }
+
+        private static void DeleteFolderContents(string folder)
+        {
+            foreach (string fileName in Directory.EnumerateFiles(folder))
+            {
+                File.Delete(fileName);
             }
         }
     }
