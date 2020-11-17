4f4685d1e6a7016ceab2f6a288a7e3151810085d
Francois de la Rouviere <fassadlr@gmail.com>
2020-11-17 12:18:29 +0000

    Update CirrusMain.cs

--- src/Stratis.Sidechains.Networks/CirrusMain.cs
+++ src/Stratis.Sidechains.Networks/CirrusMain.cs
@@ -177,7 +177,7 @@ namespace Stratis.Sidechains.Networks
             {
                 EnforceMinProtocolVersionAtBlockHeight = 384675, // setting the value to zero makes the functionality inactive
                 EnforcedMinProtocolVersion = ProtocolVersion.CIRRUS_VERSION, // minimum protocol version which will be enforced at block height defined in EnforceMinProtocolVersionAtBlockHeight
-                FederationMemberActivationTime = 1606147200 // Monday, November 23, 2020 4:00:00 PM
+                FederationMemberActivationTime = 1605862800 // Friday, November 20, 2020 9:00:00 AM
             };
 
             var buriedDeployments = new BuriedDeploymentsArray
