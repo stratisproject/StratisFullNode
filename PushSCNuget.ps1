8216a0528355953b53983d123e9aa3e6412b0dc4
Francois de la Rouviere <fassadlr@gmail.com>
2020-11-18 14:01:42 +0000

    Fixes

--- PushSCNuget.ps1
+++ PushSCNuget.ps1
@@ -1,13 +1,13 @@
-$packageNames = @("Stratis.Sidechains.Networks","Stratis.SmartContracts.CLR", "Stratis.SmartContracts.CLR.Validation", "Stratis.SmartContracts.Core", "Stratis.SmartContracts.Networks","Stratis.SmartContracts.RuntimeObserver", "Stratis.Bitcoin.Features.SmartContracts")
+$packageNames = @("Stratis.Sidechains.Networks","Stratis.SmartContracts.CLR", "Stratis.SmartContracts.CLR.Validation", "Stratis.SmartContracts.Core", "Stratis.SmartContracts.Networks","Stratis.SmartContracts.RuntimeObserver", "Stratis.Bitcoin.Features.SmartContracts", "Stratis.SmartContracts.RuntimeObserver")
 
 # A little gross to have to enter src/ and then go back after, but this is where the file is atm 
 cd "src"
 
 foreach ($packageName in $packageNames){
 	cd $packageName
-	rm "bin\debug\" -Recurse -Force -ErrorAction Ignore
-	dotnet pack --configuration Debug --include-source --include-symbols 
-	dotnet nuget push "bin\Debug\*.symbols.nupkg" --source "https://api.nuget.org/v3/index.json"
+	rm "bin\Release\" -Recurse -Force -ErrorAction Ignore
+	dotnet pack --configuration Release --include-source --include-symbols 
+	dotnet nuget push "bin\Release\*.symbols.nupkg" --source "https://api.nuget.org/v3/index.json"
 	cd ..
 }
 
