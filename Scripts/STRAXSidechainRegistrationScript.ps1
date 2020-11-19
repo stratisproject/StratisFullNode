e1ff39902a5433370a9b4f9a448c526dd3c30a2f
Iain McCain <iain.mccain@stratisplatform.com>
2020-11-19 17:29:47 +0000

    fix

--- Scripts/STRAXSidechainRegistrationScript.ps1
+++ Scripts/STRAXSidechainRegistrationScript.ps1
@@ -469,7 +469,8 @@ if ( -not ( Test-Path $sideChainDataDir\federationKey.dat ) )
     $miningDAT = Read-Host "Please Enter the full path to the federationKey.dat"
     While ( -not ( Test-Path $miningDAT ) )
     {
-        Write-Host Write-Host (Get-TimeStamp) "ERROR: $miningDAT is not a valid path.. Be sure to include the filename too '\federationKey.dat'"
+        Write-Host Write-Host (Get-TimeStamp) "ERROR: $miningDAT is not a valid path.. Be sure to include the filename too '\federationKey.dat'" -ForegroundColor Red
+        $miningDAT = Read-Host "Please Enter the full path to the federationKey.dat"
     }
     Copy-Item $miningDAT -Destination $sideChainDataDir -Force -ErrorAction Stop
 }
