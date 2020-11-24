aa3e786a70cc5a6120e3b486b2df772533ff5ec0
Francois de la Rouviere <fassadlr@gmail.com>
2020-11-24 08:49:31 +0000

    Revert "Revert "Load federation from genesis and polls (#210)" (#214)"
    This reverts commit e2ffbc42d78cc6f9eb62670a09b9e3495fe9a364.

--- src/Stratis.Features.FederatedPeg.Tests/Utils/FedPegTestsHelper.cs
+++ src/Stratis.Features.FederatedPeg.Tests/Utils/FedPegTestsHelper.cs
@@ -8,17 +8,16 @@ namespace Stratis.Features.FederatedPeg.Tests.Utils
 {
     public static class FedPegTestsHelper
     {
-        public static FederatedPegSettings CreateSettings(Network network, Network counterChainNetwork, out NodeSettings nodeSettings)
+        public static FederatedPegSettings CreateSettings(Network network, Network counterChainNetwork, string dataFolder, out NodeSettings nodeSettings)
         {
             FederationId federationId = network.Federations.GetOnlyFederation().Id;
             string redeemScript = PayToFederationTemplate.Instance.GenerateScriptPubKey(federationId).ToString();
             string federationIps = "127.0.0.1:36201,127.0.0.1:36202,127.0.0.1:36203";
             string multisigPubKey = network.Federations.GetFederation(federationId).GetFederationDetails().transactionSigningKeys.TakeLast(1).First().ToHex();
-            string[] args = new[] { "-sidechain", "-regtest", $"-federationips={federationIps}", $"-redeemscript={redeemScript}", $"-publickey={multisigPubKey}", "-mincoinmaturity=1", "-mindepositconfirmations=1" };
+            string[] args = new[] { "-sidechain", "-regtest", $"-datadir={dataFolder}", $"-federationips={federationIps}", $"-redeemscript={redeemScript}", $"-publickey={multisigPubKey}", "-mincoinmaturity=1", "-mindepositconfirmations=1" };
             nodeSettings = new NodeSettings(network, ProtocolVersion.ALT_PROTOCOL_VERSION, args: args);
 
             var settings = new FederatedPegSettings(nodeSettings, new CounterChainNetworkWrapper(counterChainNetwork));
-
             return settings;
         }
     }
