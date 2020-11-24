027abefa9caed29ff6cf1330ad0da7e1dcb7162e
Kevin Loubser <zeptin@gmail.com>
2020-11-24 12:50:57 +0200

    Name JSON fields

--- src/Stratis.Bitcoin.Features.RPC/FundRawTransactionOptions.cs
+++ src/Stratis.Bitcoin.Features.RPC/FundRawTransactionOptions.cs
@@ -1,4 +1,5 @@
 ﻿using NBitcoin;
+using Newtonsoft.Json;
 
 namespace Stratis.Bitcoin.Features.RPC
 {
@@ -7,6 +8,7 @@ namespace Stratis.Bitcoin.Features.RPC
         /// <summary>
         /// The address to receive the change.
         /// </summary>
+        [JsonProperty(PropertyName = "changeAddress")]
         public BitcoinAddress ChangeAddress
         {
             get; set;
@@ -15,6 +17,7 @@ namespace Stratis.Bitcoin.Features.RPC
         /// <summary>
         /// The index of the change output.
         /// </summary>
+        [JsonProperty(PropertyName = "changePosition")]
         public int? ChangePosition
         {
             get; set;
@@ -23,6 +26,7 @@ namespace Stratis.Bitcoin.Features.RPC
         /// <summary>
         /// The output type to use. Only valid if changeAddress is not specified. Options are "legacy", "p2sh-segwit", and "bech32"
         /// </summary>
+        [JsonProperty(PropertyName = "changeType")]
         public string ChangeType
         {
             get; set;
@@ -31,6 +35,7 @@ namespace Stratis.Bitcoin.Features.RPC
         /// <summary>
         /// Also select inputs which are watch only.
         /// </summary>
+        [JsonProperty(PropertyName = "includeWatching")]
         public bool IncludeWatching
         {
             get; set;
@@ -39,12 +44,14 @@ namespace Stratis.Bitcoin.Features.RPC
         /// <summary>
         /// Lock selected unspent outputs.
         /// </summary>
+        [JsonProperty(PropertyName = "lockUnspents")]
         public bool LockUnspents
         {
             get; set;
         }
 
         /// <remarks>Deprecated</remarks>
+        [JsonProperty(PropertyName = "reserveChangeKey")]
         public bool? ReserveChangeKey
         {
             get; set;
@@ -54,6 +61,7 @@ namespace Stratis.Bitcoin.Features.RPC
         /// Set a specific fee rate in coins/kB.
         /// Default (not set): makes wallet determine the fee.
         /// </summary>
+        [JsonProperty(PropertyName = "feeRate")]
         public FeeRate FeeRate
         {
             get; set;
@@ -65,6 +73,7 @@ namespace Stratis.Bitcoin.Features.RPC
         /// Those recipients will receive less coins than you enter in their corresponding amount field.
         /// If no outputs are specified here, the sender pays the fee.
         /// </summary>
+        [JsonProperty(PropertyName = "subtractFeeFromOutputs")]
         public int[] SubtractFeeFromOutputs
         {
             get; set;
