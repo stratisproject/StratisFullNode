4219784700a304aface9ae84a826d3612517c98c
Kevin Loubser <zeptin@gmail.com>
2020-11-24 06:56:09 +0200

    Remove duplicate files

--- src/Stratis.Bitcoin.Features.ColdStaking/Models/BuildOfflineSignRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Stratis.Bitcoin.Features.Wallet.Models;
-
-namespace Stratis.Bitcoin.Features.ColdStaking.Models
-{
-    public class BuildOfflineSignRequest : BuildTransactionRequest
-    {
-        // Currently this is identical to a normal transaction build request.
-    }
-}
