a5fe54a1ee942593c6221b04682083fc9abf8824
zeptin <zeptin@gmail.com>
2020-11-30 13:03:53 +0000

    Add consolidate endpoint and adjust subtractfee logic (#235)
    * Add consolidate endpoint and adjust subtractfee logic
    
    * Fix and augment tests
    
    * Update src/Stratis.Bitcoin.Features.Wallet/Services/WalletService.cs

--- src/Stratis.Bitcoin.Features.Wallet/Interfaces/IWalletTransactionHandler.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Interfaces/IWalletTransactionHandler.cs
@@ -40,5 +40,7 @@ namespace Stratis.Bitcoin.Features.Wallet.Interfaces
         /// <param name="context">The context that is used to build a new transaction.</param>
         /// <returns>The estimated fee.</returns>
         Money EstimateFee(TransactionBuildContext context);
+
+        int EstimateSize(TransactionBuildContext context);
     }
 }
