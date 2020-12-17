7c6174fe2976ad0cdb579f7d1fd827eac23e7fa9
Francois de la Rouviere <fassadlr@gmail.com>
2020-12-17 12:20:03 +0000

    [Reward Batching] Fix some logic when interval window skipped and better logging (#303)
    * Logging
    
    * Fix
    
    * Update RewardClaimer.cs
    
    * Update StraxTest.cs

--- src/Stratis.Bitcoin.Networks/StraxTest.cs
+++ src/Stratis.Bitcoin.Networks/StraxTest.cs
@@ -35,7 +35,7 @@ namespace Stratis.Bitcoin.Networks
             this.DefaultBanTimeSeconds = 11250; // 500 (MaxReorg) * 45 (TargetSpacing) / 2 = 3 hours, 7 minutes and 30 seconds
 
             this.CirrusRewardDummyAddress = "tGXZrZiU44fx3SQj8tAQ3Zexy2VuELZtoh";
-            this.RewardClaimerBatchActivationHeight = 150400;
+            this.RewardClaimerBatchActivationHeight = 165600;
             this.RewardClaimerBlockInterval = 10;
 
             var consensusFactory = new PosConsensusFactory();
