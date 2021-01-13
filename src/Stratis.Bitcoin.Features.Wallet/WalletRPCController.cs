6fc154dda02a7102e9b5fbb66e127ff8a2d173ad
Kevin Loubser <zeptin@gmail.com>
2021-01-13 23:06:41 +0200

    Modify getnewaddress RPC to return new address with every call

--- src/Stratis.Bitcoin.Features.Wallet/WalletRPCController.cs
+++ src/Stratis.Bitcoin.Features.Wallet/WalletRPCController.cs
@@ -425,7 +425,10 @@ namespace Stratis.Bitcoin.Features.Wallet
                 if (!addressType.Equals("legacy", StringComparison.InvariantCultureIgnoreCase))
                     throw new RPCServerException(RPCErrorCode.RPC_METHOD_NOT_FOUND, "Only address type 'legacy' is currently supported.");
             }
-            HdAddress hdAddress = this.walletManager.GetUnusedAddress(this.GetWalletAccountReference());
+
+            var walletAccountReference = this.GetWalletAccountReference();
+            var hdAddress = this.walletManager.GetNewAddresses(walletAccountReference, 1).FirstOrDefault();
+
             string base58Address = hdAddress.Address;
 
             return new NewAddressModel(base58Address);
