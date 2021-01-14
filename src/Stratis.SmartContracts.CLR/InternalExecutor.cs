2d240f0e5a3682766db4566e19752c91b66b4096
quantumagi <someguy.fromafrica@gmail.com>
2021-01-14 19:45:16 +1100

    Revert type updates to InternalExecutor

--- src/Stratis.SmartContracts.CLR/InternalExecutor.cs
+++ src/Stratis.SmartContracts.CLR/InternalExecutor.cs
@@ -22,7 +22,7 @@ namespace Stratis.SmartContracts.CLR
 
         ///<inheritdoc />
         public ICreateResult Create<T>(ISmartContractState smartContractState,
-            UInt256 amountToTransfer,
+            ulong amountToTransfer,
             object[] parameters,
             ulong gasLimit = 0)
         {
@@ -65,7 +65,7 @@ namespace Stratis.SmartContracts.CLR
         public ITransferResult Call(
             ISmartContractState smartContractState,
             Address addressTo,
-            UInt256 amountToTransfer,
+            ulong amountToTransfer,
             string methodName,
             object[] parameters,
             ulong gasLimit = 0)
@@ -104,7 +104,7 @@ namespace Stratis.SmartContracts.CLR
         }
 
         ///<inheritdoc />
-        public ITransferResult Transfer(ISmartContractState smartContractState, Address addressTo, UInt256 amountToTransfer)
+        public ITransferResult Transfer(ISmartContractState smartContractState, Address addressTo, ulong amountToTransfer)
         {
             RuntimeObserver.Gas gasRemaining = this.gasMeter.GasAvailable;
 
