bfd1a32a61ecbadad42f09a9a446d3193170a4c9
quantumagi <someguy.fromafrica@gmail.com>
2021-01-20 02:14:07 +1100

    Ensure the right Stratis.SmartContracts.Standards is used by contracts

--- src/Stratis.SmartContracts.CLR/Loader/ContractAssemblyLoader.cs
+++ src/Stratis.SmartContracts.CLR/Loader/ContractAssemblyLoader.cs
@@ -1,27 +1,47 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
+using System.Runtime.Loader;
 using CSharpFunctionalExtensions;
 
 namespace Stratis.SmartContracts.CLR.Loader
 {
+    public class SmartContractLoadContext : AssemblyLoadContext
+    {
+        private AssemblyLoadContext defaultContext;
+
+        public SmartContractLoadContext(AssemblyLoadContext defaultContext)
+        {
+            this.defaultContext = defaultContext;
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            // Ensure that an exact compatible version is used.
+            if (assemblyName.Name == "Stratis.SmartContracts.Standards" && assemblyName.Version.Major == 1 && assemblyName.Version.Minor < 4)
+                return this.LoadFromAssemblyPath(Path.GetFullPath("Stratis.SmartContracts.Standards (64 bit).dll"));
+
+            return this.defaultContext.LoadFromAssemblyName(assemblyName);
+        }
+    }
+
     /// <summary>
     /// Loads assemblies from bytecode.
     /// </summary>
-    /// <para>
-    /// TODO this may return cached assemblies in the future.
-    /// </para>
     public class ContractAssemblyLoader : ILoader
     {
         /// <summary>
-        /// Loads a contract from a raw byte array into an anonymous <see cref="AssemblyLoadContext"/>.
+        /// Loads a contract from a raw byte array into a custom <see cref="AssemblyLoadContext"/>.
         /// </summary>
         public Result<IContractAssembly> Load(ContractByteCode bytes)
         {
-            // Assembly.Load(byte[]) loads the assembly into a new anonymous AssemblyLoadContext
-            // TODO in the future, we will use a custom AssemblyLoadContext
+            // Assembly.Load(byte[]) loads the assembly into a custom AssemblyLoadContext
             try
             {
-                Assembly assembly = Assembly.Load(bytes.Value);
+                SmartContractLoadContext context = new SmartContractLoadContext(AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()));
+
+                MemoryStream s = new MemoryStream(bytes.Value);
+                Assembly assembly = context.LoadFromStream(s);
 
                 return Result.Ok<IContractAssembly>(new ContractAssembly(assembly));
             }
