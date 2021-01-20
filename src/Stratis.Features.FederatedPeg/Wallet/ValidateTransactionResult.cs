1b2bcac421a84b8f647a28a396b657316025cd61
Francois de la Rouviere <fassadlr@gmail.com>
2021-01-20 08:32:36 +0000

    Add Verify Partial Transaction Endpoint (#387)
    * Add endpoints to verify partial
    
    * Log Partials
    
    * Fix Comparer
    
    * Fix Deposit Id length
    
    * Update FederationGatewayController.cs
    
    * Update FederationGatewayController.cs

---
+++ src/Stratis.Features.FederatedPeg/Wallet/ValidateTransactionResult.cs
@@ -0,0 +1,20 @@
+﻿namespace Stratis.Features.FederatedPeg.Wallet
+{
+    public sealed class ValidateTransactionResult
+    {
+        public bool IsValid { get; set; }
+        public string[] Errors { get; set; }
+
+        private ValidateTransactionResult() { }
+
+        public static ValidateTransactionResult Failed(params string[] errors)
+        {
+            return new ValidateTransactionResult() { Errors = errors };
+        }
+
+        public static ValidateTransactionResult Valid()
+        {
+            return new ValidateTransactionResult() { IsValid = true };
+        }
+    }
+}
