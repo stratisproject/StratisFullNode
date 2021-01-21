916571901c4b4e543c5a5ed6563c9b356f3af2ac
quantumagi <someguy.fromafrica@gmail.com>
2021-01-21 20:09:55 +1100

    Log warning if logger specified

--- src/Stratis.SmartContracts.CLR/Loader/ContractAssemblyLoader.cs
+++ src/Stratis.SmartContracts.CLR/Loader/ContractAssemblyLoader.cs
@@ -6,26 +6,32 @@ using System.Net;
 using System.Reflection;
 using System.Runtime.Loader;
 using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
 
 namespace Stratis.SmartContracts.CLR.Loader
 {
     public class SmartContractLoadContext : AssemblyLoadContext
     {
-        private AssemblyLoadContext defaultContext;
+        private readonly ILogger logger;
+        private readonly AssemblyLoadContext defaultContext;
         private static Dictionary<string, byte[]> cache = new Dictionary<string, byte[]>();
 
-        public SmartContractLoadContext(AssemblyLoadContext defaultContext)
+        public SmartContractLoadContext(AssemblyLoadContext defaultContext, ILogger logger)
         {
+            this.logger = logger;
             this.defaultContext = defaultContext;
         }
 
-        public static string GetExactAssembly(AssemblyName assemblyName)
+        public static string GetExactAssembly(AssemblyName assemblyName, out string errorMessage)
         {
             // The DLL is not included with our distribution. See if we can get it from NuGet.org.
             string version = assemblyName.Version.ToString();
             string folderName = $"{assemblyName.Name.ToLower()}.{version}";
             string assemblyFolder = Path.GetFullPath("LegacyStandardsDLLs");
             string assemblyPath = Path.Combine(assemblyFolder, folderName);
+
+            errorMessage = null;
+
             if (!Directory.Exists(assemblyPath))
             {
                 string downloadFile = $"{assemblyPath}.nupkg";
@@ -45,7 +51,8 @@ namespace Stratis.SmartContracts.CLR.Loader
                     }
                     catch (Exception)
                     {
-                        throw new FileNotFoundException($"Could not find '{downloadFile}'. Get the file from '{downloadLink}' and copy it to this location.");
+                        errorMessage = $"Could not find '{downloadFile}'. Get the file from '{downloadLink}' and copy it to this location.";
+                        return null;
                     }
                 }
 
@@ -63,9 +70,23 @@ namespace Stratis.SmartContracts.CLR.Loader
             {
                 if (!cache.TryGetValue(assemblyName.FullName, out byte[] bytes))
                 {
-                    bytes = File.ReadAllBytes(GetExactAssembly(assemblyName));
+                    string exactAssembly = GetExactAssembly(assemblyName, out string error);
+                    if (exactAssembly == null)
+                    {
+                        if (this.logger == null)
+                            throw new Exception(error);
+
+                        cache[assemblyName.FullName] = null;
+                        this.logger.LogWarning(error);
+                        return this.defaultContext.LoadFromAssemblyName(assemblyName);
+                    }
+
+                    bytes = File.ReadAllBytes(exactAssembly);
                     cache[assemblyName.FullName] = bytes;
                 }
+
+                if (bytes == null)
+                    return this.defaultContext.LoadFromAssemblyName(assemblyName);
 
                 using (var stream = new MemoryStream(bytes))
                 {
@@ -82,6 +103,13 @@ namespace Stratis.SmartContracts.CLR.Loader
     /// </summary>
     public class ContractAssemblyLoader : ILoader
     {
+        private readonly ILogger logger;
+
+        public ContractAssemblyLoader(ILoggerFactory loggerFactory = null)
+        {
+            this.logger = loggerFactory?.CreateLogger(typeof(ContractAssemblyLoader).Name);
+        }
+
         /// <summary>
         /// Loads a contract from a raw byte array into a custom <see cref="AssemblyLoadContext"/>.
         /// </summary>
@@ -90,7 +118,7 @@ namespace Stratis.SmartContracts.CLR.Loader
             // Assembly.Load(byte[]) loads the assembly into a custom AssemblyLoadContext
             try
             {
-                SmartContractLoadContext context = new SmartContractLoadContext(AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()));
+                SmartContractLoadContext context = new SmartContractLoadContext(AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()), this.logger) ;
 
                 MemoryStream s = new MemoryStream(bytes.Value);
                 Assembly assembly = context.LoadFromStream(s);
