cce56a57a68f3c0dd7b32bf8c6afb6778192aee4
quantumagi <someguy.fromafrica@gmail.com>
2021-01-28 18:36:06 +1100

    Determine why finality is breaking

--- src/Stratis.Bitcoin.Features.Consensus/Rules/PosConsensusRuleEngine.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Rules/PosConsensusRuleEngine.cs
@@ -30,14 +30,18 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules
 
         public IRewindDataIndexCache RewindDataIndexCache { get; }
 
+        private readonly IFinalizedBlockInfoRepository finalizedBlockInfoRepository;
+
         public PosConsensusRuleEngine(Network network, ILoggerFactory loggerFactory, IDateTimeProvider dateTimeProvider, ChainIndexer chainIndexer, NodeDeployments nodeDeployments,
             ConsensusSettings consensusSettings, ICheckpoints checkpoints, ICoinView utxoSet, IStakeChain stakeChain, IStakeValidator stakeValidator, IChainState chainState,
-            IInvalidBlockHashStore invalidBlockHashStore, INodeStats nodeStats, IRewindDataIndexCache rewindDataIndexCache, IAsyncProvider asyncProvider, ConsensusRulesContainer consensusRulesContainer)
+            IInvalidBlockHashStore invalidBlockHashStore, INodeStats nodeStats, IRewindDataIndexCache rewindDataIndexCache, IAsyncProvider asyncProvider, ConsensusRulesContainer consensusRulesContainer,
+            IFinalizedBlockInfoRepository finalizedBlockInfoRepository = null)
             : base(network, loggerFactory, dateTimeProvider, chainIndexer, nodeDeployments, consensusSettings, checkpoints, utxoSet, chainState, invalidBlockHashStore, nodeStats, asyncProvider, consensusRulesContainer)
         {
             this.StakeChain = stakeChain;
             this.StakeValidator = stakeValidator;
             this.RewindDataIndexCache = rewindDataIndexCache;
+            this.finalizedBlockInfoRepository = finalizedBlockInfoRepository;
         }
 
         /// <inheritdoc />
@@ -54,10 +58,20 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules
 
             this.StakeChain.Load();
 
+            // Is the chain tip ok?
+            // TODO: This is temporary code to give an earlier indication of why this error is raised in the Initialize call below. 
+            if (this.finalizedBlockInfoRepository != null && chainTip.Height < this.finalizedBlockInfoRepository.GetFinalizedBlockInfo().Height)
+                throw new ConsensusException($"Violation of finality on height { chainTip.Height } for RewindDataIndex.");
+
             // A temporary hack until tip manage will be introduced.
             var coindb = ((CachedCoinView)this.UtxoSet).ICoindb;
             HashHeightPair hash = coindb.GetTipHash();
             ChainedHeader tip = chainTip.FindAncestorOrSelf(hash.Hash);
+
+            // Determine if the hack is leading to issues.
+            // TODO: This is temporary code to give an earlier indication of why this error is raised in the Initialize call below. 
+            if (this.finalizedBlockInfoRepository != null && tip.Height < this.finalizedBlockInfoRepository.GetFinalizedBlockInfo().Height)
+                throw new ConsensusException($"Violation of finality on height { tip.Height } for RewindDataIndex.");
 
             this.RewindDataIndexCache.Initialize(tip.Height, this.UtxoSet);
         }
