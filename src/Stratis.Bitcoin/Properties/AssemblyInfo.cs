b111c9a48886e6e873509600cf657183151df9ee
Francois de la Rouviere <fassadlr@gmail.com>
2021-02-05 16:25:46 +0000

    Bump Version to 1.0.6.5

--- src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
+++ src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stratis Group Ltd.")]
 [assembly: AssemblyProduct("Stratis Full Node")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.4")]
-[assembly: AssemblyFileVersion("1.0.6.4")]
+[assembly: AssemblyVersion("1.0.6.5")]
+[assembly: AssemblyFileVersion("1.0.6.5")]
 [assembly: InternalsVisibleTo("Stratis.Bitcoin.Tests")]
