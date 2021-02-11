38959482cb47c1a506db8ebc9751bf328f6fecbc
quantumagi <someguy.fromafrica@gmail.com>
2021-02-11 16:13:09 +1100

    Refactor

--- src/Stratis.Bitcoin.Tests/PackagesAndVersions/PackagesAndVersionsTests.cs
+++ src/Stratis.Bitcoin.Tests/PackagesAndVersions/PackagesAndVersionsTests.cs
@@ -29,14 +29,23 @@ namespace Stratis.Bitcoin.Tests.PackagesAndVersions
             SolutionFile solutionFile = SolutionFile.Parse(solutionFilePath);
             HashSet<string> extra = new HashSet<string>() { "NBitcoin", "FodyNlogAdapter" };
 
-            foreach (ProjectInSolution project in solutionFile.ProjectsByGuid.Values)
+            var projectsByPath = solutionFile.ProjectsByGuid
+                .Where(p => p.Value.ProjectName.StartsWith("Stratis.") || extra.Contains(p.Value.ProjectName))
+                .ToDictionary(p => p.Value.AbsolutePath, p => p.Value);
+            var projectFiles = projectsByPath.ToDictionary(p => p.Key, p => { XmlDocument doc = new XmlDocument(); doc.Load(p.Key); return doc; });
+            var referencedVersions = projectFiles.ToDictionary(p => p.Key, p => p.Value.SelectSingleNode("Project/PropertyGroup/Version")?.InnerText);
+            var projectsToCheck = new List<string>(projectsByPath.Keys);
+
+            while (projectsToCheck.Count > 0)
             {
-                if (!project.ProjectName.StartsWith("Stratis.") && !extra.Contains(project.ProjectName))
-                    continue;
+                string projectFolder = projectsToCheck.First();
+                projectsToCheck.RemoveAt(0);
+
+                ProjectInSolution project = projectsByPath[projectFolder];
 
                 // Read project file.
-                XmlDocument doc = new XmlDocument();
-                doc.Load(project.AbsolutePath);
+                XmlDocument doc = projectFiles[projectFolder];
+
                 string name = doc.SelectSingleNode("Project/PropertyGroup/PackageId")?.InnerText;
                 if (name == null)
                     continue;
@@ -48,13 +57,34 @@ namespace Stratis.Bitcoin.Tests.PackagesAndVersions
                 if (version.EndsWith(".0"))
                     version = version.Substring(0, version.Length - 2);
 
-                string projectFolder = Path.GetDirectoryName(project.AbsolutePath);
+                // Check the referenced projects first.
+                var references = new List<string>();
+                foreach (XmlNode x in doc.SelectNodes("/Project/ItemGroup/ProjectReference"))
+                {
+                    string includePath = x.Attributes["Include"].Value;
+                    string referencedProject = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(projectFolder), includePath));
+
+                    if (projectsToCheck.Contains(referencedProject))
+                    {
+                        references.Add(referencedProject);
+                        projectsToCheck.Remove(referencedProject);
+                    }
+                }
+
+                if (references.Count > 0)
+                {
+                    projectsToCheck.InsertRange(0, references);
+                    projectsToCheck.Add(projectFolder);
+                    continue;
+                }
+
                 string targetName = $"{name.ToLower()}.{version}-nuget.symbols";
-                string targetFolder = Path.Combine(projectFolder, "bin", "Release");
+                string targetFolder = Path.Combine(Path.GetDirectoryName(projectFolder), "bin", "Release");
                 string targetPath = Path.Combine(targetFolder, targetName);
                 if (!Directory.Exists(targetPath))
                 {
-                    Directory.CreateDirectory(targetFolder);
+                    if (!Directory.Exists(targetFolder))
+                        Directory.CreateDirectory(targetFolder);
 
                     string targetFile = Path.Combine(projectFolder, $"{targetPath}.nupkg");
                     if (!File.Exists(targetFile))
@@ -82,10 +112,9 @@ namespace Stratis.Bitcoin.Tests.PackagesAndVersions
 
                 // Compare source with files from NuGet.
                 string packageSource = Path.Combine(targetPath, "src", project.ProjectName);
-                if (Directory.Exists(packageSource) && DirectoryEquals(packageSource, projectFolder))
+                if (Directory.Exists(packageSource) && DirectoryEquals(packageSource, Path.GetDirectoryName(projectFolder)))
                 {
-                    // Even though the project file may be unchanged the references could be referring
-                    // to different versions.
+                    // Even though the project file may be unchanged the references could be referring to different versions.
 
                     // Load the NUSPEC file from the package.
                     string nuspecFile = Path.Combine(targetPath, $"{name}.nuspec");
@@ -96,9 +125,9 @@ namespace Stratis.Bitcoin.Tests.PackagesAndVersions
                     foreach (XmlNode x in doc.SelectNodes("/Project/ItemGroup/ProjectReference"))
                     {
                         string includePath = x.Attributes["Include"].Value;
+                        string includeFullPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(projectFolder), includePath));
 
-                        XmlDocument doc3 = new XmlDocument();
-                        doc3.Load(Path.Combine(projectFolder, includePath));
+                        XmlDocument doc3 = projectFiles[includeFullPath];
                         string name3 = doc3.SelectSingleNode("Project/PropertyGroup/PackageId")?.InnerText;
                         string version3 = doc3.SelectSingleNode("Project/PropertyGroup/Version")?.InnerText;
 
