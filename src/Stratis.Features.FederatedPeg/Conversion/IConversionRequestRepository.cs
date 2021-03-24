c2303cd9297bc7e264379ea998e4dab245006aa7
noescape00 <NoEscape0@ya.ru>
2021-03-24 16:14:06 +0300

    htmldoc for IConversionRequestRepository

--- src/Stratis.Features.FederatedPeg/Conversion/IConversionRequestRepository.cs
+++ src/Stratis.Features.FederatedPeg/Conversion/IConversionRequestRepository.cs
@@ -2,14 +2,19 @@
 
 namespace Stratis.Bitcoin.Features.FederatedPeg
 {
+    /// <summary>Provides saving and retrieving functionality for objects of <see cref="ConversionRequest"/> type.</summary>
     public interface IConversionRequestRepository
     {
+        /// <summary>Saves <see cref="ConversionRequest"/> to the repository.</summary>
         void Save(ConversionRequest request);
 
+        /// <summary>Retrieves <see cref="ConversionRequest"/> with specified id.</summary>
         ConversionRequest Get(string requestId);
 
+        /// <summary>Retrieves all mint requests.</summary>
         List<ConversionRequest> GetAllMint(bool onlyUnprocessed);
 
+        /// <summary>Retrieves all burn requests.</summary>
         List<ConversionRequest> GetAllBurn(bool onlyUnprocessed);
     }
 }
