2ffef7702ab2e527500018bad5ab8d405bac3144
quantumagi <someguy.fromafrica@gmail.com>
2021-03-30 16:54:03 +1100

    Candidate 2

---
+++ src/Stratis.SmartContracts.CLR.Tests/SmartContracts/SystemContractsDictionary.cs
@@ -0,0 +1,125 @@
+﻿using Stratis.SmartContracts;
+
+public struct WhiteListEntry
+{
+    public UInt256 CodeHash;
+    public Address LastAddress;
+    public string Name;
+}
+
+public class SystemContractsDictionary : SmartContract
+{
+    public SystemContractsDictionary(ISmartContractState state) : base(state)
+    {
+    }
+
+    public bool IsWhiteListed(UInt256 codeHash)
+    {
+        Assert(codeHash != default(UInt256));
+
+        WhiteListEntry whiteListEntry = this.State.GetStruct<WhiteListEntry>(codeHash.ToString());
+
+        return whiteListEntry.CodeHash != default(UInt256);
+    }
+
+    public UInt256 GetCodeHash(string name)
+    {
+        Assert(!string.IsNullOrEmpty(name));
+
+        return this.State.GetUInt256($"ByName:{name}");
+    }
+
+    public Address GetContractAddress(string name)
+    {
+        Assert(!string.IsNullOrEmpty(name));
+
+        UInt256 codeHash = this.State.GetUInt256($"ByName:{name}");
+
+        if (codeHash == default(UInt256))
+            return default(Address);
+
+        WhiteListEntry whiteListEntry = this.State.GetStruct<WhiteListEntry>(codeHash.ToString());
+
+        return whiteListEntry.LastAddress;
+    }
+
+    public Address GetContractAddress(UInt256 codeHash)
+    {
+        Assert(codeHash != default(UInt256));
+
+        WhiteListEntry whiteListEntry = this.State.GetStruct<WhiteListEntry>(codeHash.ToString());
+
+        return whiteListEntry.LastAddress;
+    }
+
+    public void WhiteList(byte[] signatures, UInt256 codeHash, Address lastAddress, string name)
+    {
+        Assert(signatures != null);
+        Assert(codeHash != default(UInt256));
+
+        UInt256 codeHashKey = codeHash;
+        WhiteListEntry whiteListEntry;
+
+        if (!string.IsNullOrEmpty(name))
+        {
+            codeHashKey = this.State.GetUInt256($"ByName:{name}");
+
+            if (codeHashKey == default(UInt256))
+                codeHashKey = codeHash;
+        }
+
+        whiteListEntry = this.State.GetStruct<WhiteListEntry>(codeHashKey.ToString());
+
+        uint nonce = this.State.GetUInt32($"Nonce:{codeHash}");
+
+        string authorizationChallenge;
+        if (whiteListEntry.CodeHash == default(UInt256))
+        {
+            authorizationChallenge = $"WhiteList(Nonce:{nonce},CodeHash:{codeHash},LastAddress:{lastAddress},Name:{name})";
+        }
+        else
+        {
+            Assert(whiteListEntry.CodeHash != codeHash || whiteListEntry.LastAddress != lastAddress || whiteListEntry.Name != name, "Nothing changed.");
+            authorizationChallenge = $"WhiteList(Nonce:{nonce},CodeHash:{whiteListEntry.CodeHash}=>{codeHash},LastAddress:{whiteListEntry.LastAddress}=>{lastAddress},Name:{whiteListEntry.Name}=>{name})";
+        }
+
+        //this.VerifySignatures(authorizationChallenge, signatures);
+
+        if (whiteListEntry.CodeHash != default(UInt256))
+        {
+            if (codeHash != whiteListEntry.CodeHash)
+                this.State.Clear(whiteListEntry.CodeHash.ToString());
+
+            if (string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(whiteListEntry.Name))
+                this.State.Clear($"ByName:{whiteListEntry.Name}");
+        }
+
+        whiteListEntry.CodeHash = codeHash;
+        whiteListEntry.LastAddress = lastAddress;
+        whiteListEntry.Name = name;
+
+        this.State.SetStruct<WhiteListEntry>(codeHash.ToString(), whiteListEntry);
+        this.State.SetUInt256($"ByName:{name}", codeHash);
+        this.State.SetUInt32($"Nonce:{codeHash}", nonce + 1);
+    }
+
+    public void BlackList(byte[] signatures, UInt256 codeHash)
+    {
+        Assert(signatures != null);
+        Assert(codeHash != default(UInt256));
+
+        WhiteListEntry whiteListEntry = this.State.GetStruct<WhiteListEntry>(codeHash.ToString());
+
+        Assert(whiteListEntry.CodeHash != default(UInt256));
+
+        uint nonce = this.State.GetUInt32($"Nonce:{codeHash}");
+
+        string authorizationChallenge = $"BlackList(Nonce:{nonce},CodeHash:{whiteListEntry.CodeHash},LastAddress:{whiteListEntry.LastAddress},Name:{whiteListEntry.Name})";
+
+        //this.VerifySignatures(authorizationChallenge, signatures);
+
+        this.State.Clear(codeHash.ToString());
+        this.State.Clear($"ByName:{whiteListEntry.Name}");
+        this.State.SetUInt32($"Nonce:{codeHash}", nonce + 1);
+    }
+}
---
+++ src/Stratis.SmartContracts.CLR.Tests/SystemContractDictionaryTests.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NBitcoin;
+using Stratis.Bitcoin.Configuration.Logging;
+using Stratis.Patricia;
+using Stratis.SmartContracts.CLR.Caching;
+using Stratis.SmartContracts.CLR.Compilation;
+using Stratis.SmartContracts.CLR.Loader;
+using Stratis.SmartContracts.CLR.ResultProcessors;
+using Stratis.SmartContracts.CLR.Serialization;
+using Stratis.SmartContracts.CLR.Validation;
+using Stratis.SmartContracts.Core;
+using Stratis.SmartContracts.Core.State;
+using Stratis.SmartContracts.Networks;
+using Xunit;
+
+
+namespace Stratis.SmartContracts.CLR.Tests
+{
+    public class SystemContractDictionaryTests
+    {
+        private const ulong BlockHeight = 0;
+        private static readonly uint160 CoinbaseAddress = 0;
+        private static readonly uint160 ToAddress = 1;
+        private static readonly uint160 SenderAddress = 2;
+        private static readonly Money MempoolFee = new Money(1_000_000);
+        private readonly IKeyEncodingStrategy keyEncodingStrategy;
+        private readonly ILoggerFactory loggerFactory;
+        private readonly Network network;
+        private readonly IContractRefundProcessor refundProcessor;
+        private readonly IStateRepositoryRoot state;
+        private readonly IContractTransferProcessor transferProcessor;
+        private readonly SmartContractValidator validator;
+        private IInternalExecutorFactory internalTxExecutorFactory;
+        private readonly IContractAssemblyCache contractCache;
+        private IVirtualMachine vm;
+        private readonly ICallDataSerializer callDataSerializer;
+        private readonly StateFactory stateFactory;
+        private readonly IAddressGenerator addressGenerator;
+        private readonly ILoader assemblyLoader;
+        private readonly IContractModuleDefinitionReader moduleDefinitionReader;
+        private readonly IContractPrimitiveSerializer contractPrimitiveSerializer;
+        private readonly IStateProcessor stateProcessor;
+        private readonly ISmartContractStateFactory smartContractStateFactory;
+        private readonly ISerializer serializer;
+
+        public SystemContractDictionaryTests()
+        {
+            this.keyEncodingStrategy = BasicKeyEncodingStrategy.Default;
+            this.loggerFactory = ExtendedLoggerFactory.Create();
+            this.network = new SmartContractsRegTest();
+            this.refundProcessor = new ContractRefundProcessor(this.loggerFactory);
+            this.state = new StateRepositoryRoot(new NoDeleteSource<byte[], byte[]>(new MemoryDictionarySource()));
+            this.transferProcessor = new ContractTransferProcessor(this.loggerFactory, this.network);
+            this.validator = new SmartContractValidator();
+            this.addressGenerator = new AddressGenerator();
+            this.assemblyLoader = new ContractAssemblyLoader();
+            this.moduleDefinitionReader = new ContractModuleDefinitionReader();
+            this.contractPrimitiveSerializer = new ContractPrimitiveSerializer(this.network);
+            this.serializer = new Serializer(this.contractPrimitiveSerializer);
+            this.contractCache = new ContractAssemblyCache();
+            this.vm = new ReflectionVirtualMachine(this.validator, this.loggerFactory, this.assemblyLoader, this.moduleDefinitionReader, this.contractCache);
+            this.stateProcessor = new StateProcessor(this.vm, this.addressGenerator);
+            this.internalTxExecutorFactory = new InternalExecutorFactory(this.loggerFactory, this.stateProcessor);
+            this.smartContractStateFactory = new SmartContractStateFactory(this.contractPrimitiveSerializer, this.internalTxExecutorFactory, this.serializer);
+
+            this.callDataSerializer = new CallDataSerializer(this.contractPrimitiveSerializer);
+
+            this.stateFactory = new StateFactory(this.smartContractStateFactory);
+        }
+
+        [Fact]
+        public void CanCompileSystemContractDictionary()
+        {
+            ContractCompilationResult compilationResult = ContractCompiler.CompileFile("SmartContracts/SystemContractsDictionary.cs");
+            Assert.True(compilationResult.Success);
+            byte[] contractCode = compilationResult.Compilation;
+
+            var contractTxData = new ContractTxData(0, (RuntimeObserver.Gas)1, (RuntimeObserver.Gas)500_000, contractCode);
+            var tx = new Transaction();
+            tx.AddOutput(0, new Script(this.callDataSerializer.Serialize(contractTxData)));
+
+            IContractTransactionContext transactionContext = new ContractTransactionContext(BlockHeight, CoinbaseAddress, MempoolFee, new uint160(2), tx);
+
+            var executor = new ContractExecutor(
+                this.callDataSerializer,
+                this.state,
+                this.refundProcessor,
+                this.transferProcessor,
+                this.stateFactory,
+                this.stateProcessor,
+                this.contractPrimitiveSerializer);
+
+            IContractExecutionResult result = executor.Execute(transactionContext);
+
+            Assert.Null(result.ErrorMessage);
+        }
+
+        [Fact]
+        public void CanWhitelistSystemContracts()
+        {
+            var internalTxExecutor = new Mock<IInternalTransactionExecutor>();
+            var internalHashHelper = new Mock<IInternalHashHelper>();
+            var persistentState = new TestPersistentState();
+            var block = new Mock<IBlock>();
+            var message = new Mock<IMessage>();
+            Func<ulong> getBalance = () => 1;
+
+            ISmartContractState state = Mock.Of<ISmartContractState>(
+                g => g.InternalTransactionExecutor == internalTxExecutor.Object
+                     && g.InternalHashHelper == internalHashHelper.Object
+                     && g.PersistentState == persistentState
+                     && g.Block == block.Object
+                     && g.Message == message.Object
+                     && g.GetBalance == getBalance);
+
+            IContract contract = Contract.CreateUninitialized(typeof(SystemContractsDictionary), state, new uint160(2));
+            var instance = (SystemContractsDictionary)contract.GetPrivateFieldValue("instance");
+
+            // TODO: Verify signatures.
+            byte[] signatures = new byte[] { 0 };
+            UInt256 codeHash = 1;
+            string name = "Name";
+            Address address = new Address(0, 0, 0, 0, 1);
+
+            var callWhiteList = new MethodCall("WhiteList", new object[] { signatures, codeHash, address, name });
+            IContractInvocationResult resultWhiteList = contract.Invoke(callWhiteList);
+            Assert.True(resultWhiteList.IsSuccess);
+
+            WhiteListEntry whiteListEntry = persistentState.GetStruct<WhiteListEntry>(codeHash.ToString());
+
+            Assert.Equal(name, whiteListEntry.Name);
+            Assert.Equal(codeHash, whiteListEntry.CodeHash);
+            Assert.Equal(address, whiteListEntry.LastAddress);
+
+            Assert.Equal(codeHash, persistentState.GetUInt256($"ByName:{name}"));
+
+            var callIsWhiteListed = new MethodCall("IsWhiteListed", new object[] { codeHash });
+            IContractInvocationResult resultIsWhiteListed = contract.Invoke(callIsWhiteListed);
+            Assert.True((bool)resultIsWhiteListed.Return);
+
+            var callGetCodeHash = new MethodCall("GetCodeHash", new object[] { name });
+            IContractInvocationResult resultGetCodeHash = contract.Invoke(callGetCodeHash);
+            Assert.Equal(codeHash, (UInt256)resultGetCodeHash.Return);
+
+            var callGetContractAddress = new MethodCall("GetContractAddress", new object[] { name });
+            IContractInvocationResult resultGetContractAddress = contract.Invoke(callGetContractAddress);
+            Assert.Equal(address, (Address)resultGetContractAddress.Return);
+
+            var callGetContractAddressCH = new MethodCall("GetContractAddress", new object[] { codeHash });
+            IContractInvocationResult resultGetContractAddressCH = contract.Invoke(callGetContractAddressCH);
+            Assert.Equal(address, (Address)resultGetContractAddressCH.Return);
+
+            var callBlackList = new MethodCall("BlackList", new object[] { signatures, codeHash });
+            IContractInvocationResult resultBlackList = contract.Invoke(callBlackList);
+            Assert.True(resultBlackList.IsSuccess);
+
+            var callIsWhiteListed2 = new MethodCall("IsWhiteListed", new object[] { codeHash });
+            IContractInvocationResult resultIsWhiteListed2 = contract.Invoke(callIsWhiteListed2);
+            Assert.False((bool)resultIsWhiteListed2.Return);
+
+            Assert.Equal(codeHash, persistentState.GetUInt256($"ByName:{name}"));
+
+            // TODO: Should these methods return anything once the conteact is black-listed?
+
+            var callGetCodeHash2 = new MethodCall("GetCodeHash", new object[] { name });
+            IContractInvocationResult resultGetCodeHash2 = contract.Invoke(callGetCodeHash2);
+            Assert.Equal(codeHash, (UInt256)resultGetCodeHash2.Return);
+
+            var callGetContractAddress2 = new MethodCall("GetContractAddress", new object[] { name });
+            IContractInvocationResult resultGetContractAddress2 = contract.Invoke(callGetContractAddress2);
+            Assert.Equal(address, (Address)resultGetContractAddress2.Return);
+
+            var callGetContractAddressCH2 = new MethodCall("GetContractAddress", new object[] { codeHash });
+            IContractInvocationResult resultGetContractAddressCH2 = contract.Invoke(callGetContractAddressCH2);
+            Assert.Equal(address, (Address)resultGetContractAddressCH2.Return);
+        }
+    }
+}
