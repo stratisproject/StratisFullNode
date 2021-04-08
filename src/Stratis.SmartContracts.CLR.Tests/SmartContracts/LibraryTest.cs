5a00154fd7d9e18e51ce0cc407f1bf6be8e1d27b
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-08 17:04:32 +1000

    Add tests

---
+++ src/Stratis.SmartContracts.CLR.Tests/SmartContracts/LibraryTest.cs
@@ -0,0 +1,16 @@
+﻿using Stratis.SmartContracts;
+using Base = Stratis.SCL.Base;
+
+[Deploy]
+public class LibraryTest : SmartContract
+{
+    public LibraryTest(ISmartContractState state) : base(state)
+    {
+        Base.Operations.Noop();
+    }
+
+    public void Exists()
+    {
+        State.SetBool("Exists", true);
+    }
+}
