546abc4b071c5c70d79829f08ac8e8fee7a5f920
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-08 19:00:39 +1000

    Add EcRecover integration tests

---
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContracts/EcRecoverContract.cs
@@ -0,0 +1,28 @@
+﻿using Stratis.SmartContracts;
+using EcRecover = Stratis.SCL.Crypto.ECRecover;
+
+public class EcRecoverContract : SmartContract
+{
+    public Address ThirdPartySigner
+    {
+        get
+        {
+            return this.State.GetAddress(nameof(this.ThirdPartySigner));
+        }
+        set
+        {
+            this.State.SetAddress(nameof(this.ThirdPartySigner), value);
+        }
+    }
+
+    public EcRecoverContract(ISmartContractState state, Address thirdPartySigner) : base(state)
+    {
+        this.ThirdPartySigner = thirdPartySigner;
+    }
+
+    public bool CheckThirdPartySignature(byte[] message, byte[] signature)
+    {
+        Address signerOfMessage = EcRecover.GetSigner(message, signature);
+        return (signerOfMessage == this.ThirdPartySigner);
+    }
+}
