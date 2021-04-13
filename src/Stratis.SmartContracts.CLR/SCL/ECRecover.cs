0b615112db89043b220f15279b18c8e5440630e3
quantumagi <someguy.fromafrica@gmail.com>
2021-04-13 14:36:54 +1000

    Changes based on feedback

--- src/Stratis.SmartContracts.CLR/SCL/ECRecover.cs
+++ src/Stratis.SmartContracts.CLR/SCL/ECRecover.cs
@@ -1,34 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NBitcoin;
 using NBitcoin.DataEncoders;
 using Stratis.SmartContracts;
 using Stratis.SCL.Base;
+using Stratis.SmartContracts.CLR;
 
 namespace Stratis.SCL.Crypto
 {
     public static class ECRecover
     {
+        private static Address[] VerifySignatures(byte[] signatures, byte[] message, Address[] addresses)
+        {
+            if (message == null || signatures == null)
+                return null;
+
+            const int signatureLength = 65;
+            const int minHeaderByte = 27;
+            const int maxHeaderByte = 34;
+
+            byte[][] sigArray = Operations.DeflattenByteArray(signatures, signatureLength);
+            if (sigArray == null)
+                return null;
+
+            if (sigArray.Any(s => s.Length == 0 || s[0] < minHeaderByte || s[0] > maxHeaderByte))
+                return null;
+
+            IEnumerable<KeyId> keyIds = sigArray.Select(s => PubKey.RecoverFromMessage(message, Encoders.Base64.EncodeData(s)).Hash);
+
+            if (addresses != null)
+                keyIds = keyIds.Intersect(addresses.Select(a => new KeyId(a.ToBytes())));
+
+            return keyIds.Select(s => s.ToBytes().ToAddress()).ToArray();
+        }
+
         /// <summary>
         /// Takes a message and signatures and recovers the list of addresses that produced the signatures.
         /// </summary>
         /// <param name="signatures">The list of signatures of the message.</param>
         /// <param name="message">The message that was signed.</param>
-        /// <param name="addresses">If not <c>null</c> the addresses returned are intersected with these addresses.</param>
-        /// <returns>The list of addresses that produced the signatures and constrained to the list provided in <paramref name="addresses"/> if not <c>null</c>.</returns>
-        public static Address[] VerifySignatures(byte[] signatures, byte[] message, Address[] addresses = null)
+        /// <param name="addresses">The addresses returned are intersected with these addresses.</param>
+        /// <param name="verifiedAddresses">The list of addresses that produced the signatures and constrained to the list provided in <paramref name="addresses"/>.</param>
+        /// <returns>The boolean value returned only indicates whether the operation could be performed. The number of verified addresses should still be checked.</returns>
+        public static bool TryVerifySignatures(byte[] signatures, byte[] message, Address[] addresses, out Address[] verifiedAddresses)
         {
-            if (message == null || signatures == null)
-                return null;
+            if (addresses == null)
+            {
+                verifiedAddresses = null;
+                return false;
+            }
 
-            IEnumerable<KeyId> keyIds = Operations.DeserializeSignatures(signatures)
-                .Select(s => PubKey.RecoverFromMessage(message, Encoders.Base64.EncodeData(s)).Hash);
+            verifiedAddresses = VerifySignatures(signatures, message, addresses);
 
-            if (addresses != null)
-                keyIds = keyIds.Intersect(addresses.Select(a => new KeyId(a.ToBytes())));
-
-            return keyIds.Select(s => Operations.DeserializeAddress(s.ToBytes())).ToArray();
+            return verifiedAddresses != null;
         }
     }
 }
--- src/Stratis.SmartContracts.CLR/SCL/Operations.cs
+++ src/Stratis.SmartContracts.CLR/SCL/Operations.cs
@@ -1,35 +1,19 @@
 ﻿using System;
-using Stratis.SmartContracts;
 
 namespace Stratis.SCL.Base
 {
     public static class Operations
     {
-        public static Address DeserializeAddress(byte[] address)
+        public static byte[][] DeflattenByteArray(byte[] array, int subArrayLength)
         {
-            var pn = new uint[5];
-            for (int i = 0, offs = 0; i < pn.Length; i++, offs += 4)
-                pn[i] = BitConverter.ToUInt32(address, offs);
-            return new Address(pn[0], pn[1], pn[2], pn[3], pn[4]);
-        }
+            int cnt = array.Length / subArrayLength;
 
-        public static byte[][] DeserializeSignatures(byte[] signatures)
-        {
-            const int signatureLength = 65;
-            const int minHeaderByte = 27;
-            const int maxHeaderByte = 34;
-            int cnt = signatures.Length / signatureLength;
-
-            if (signatures.Length != signatureLength * cnt)
+            if (array.Length != subArrayLength * cnt)
                 return null;
 
             var buffer = new byte[cnt][];
             for (int i = 0; i < cnt; i++)
-            {
-                Array.Copy(signatures, i * signatureLength, buffer[i], 0, signatureLength);
-                if (buffer[i][0] < minHeaderByte || buffer[i][0] > maxHeaderByte)
-                    return null;
-            }
+                Array.Copy(array, i * subArrayLength, buffer[i], 0, subArrayLength);
 
             return buffer;
         }
