4af155fb9394086f6de674210a82c97c34186c2c
quantumagi <someguy.fromafrica@gmail.com>
2021-04-13 15:35:18 +1000

    Add types

--- src/Stratis.SmartContracts.CLR.Tests/SCL/ECRecoverTests.cs
+++ src/Stratis.SmartContracts.CLR.Tests/SCL/ECRecoverTests.cs
@@ -12,12 +12,12 @@ namespace Stratis.SmartContracts.CLR.Tests.SCL
         [Fact]
         public void CanVerifySignatures()
         {
-            var authorizationChallenge = "This is a test";
-            var keys = new[] { new Key(), new Key(), new Key() };
-            var sigs = keys.Select(k => Convert.FromBase64String(k.SignMessage(authorizationChallenge))).ToArray();
-            var addresses = keys.Select(k => k.PubKey.Hash.ToBytes().ToAddress()).ToArray();
+            string authorizationChallenge = "This is a test";
+            Key[] keys = new[] { new Key(), new Key(), new Key() };
+            Address[] addresses = keys.Select(k => k.PubKey.Hash.ToBytes().ToAddress()).ToArray();
 
-            var flatSigs = Operations.FlattenArray(sigs, sigs[0].Length);
+            byte[][] sigs = keys.Select(k => Convert.FromBase64String(k.SignMessage(authorizationChallenge))).ToArray();
+            byte[] flatSigs = Operations.FlattenArray(sigs, sigs[0].Length);
 
             Assert.True(ECRecover.TryVerifySignatures(flatSigs, System.Text.Encoding.ASCII.GetBytes(authorizationChallenge), addresses, out Address[] verifiedAddresses));
 
