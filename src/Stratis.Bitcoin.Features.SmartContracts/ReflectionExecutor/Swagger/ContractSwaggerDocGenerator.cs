ba9be89b77ecf187b643e7fca6a1ee2355f33b31
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-15 17:32:18 +1000

    Add stub ContractSwaggerDocGenerator

---
+++ src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Swagger/ContractSwaggerDocGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Swagger
+{
+    public class ContractSwaggerDocGenerator : ISwaggerProvider
+    {
+        public OpenApiDocument GetSwagger(string documentName, string host = null, string basePath = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
