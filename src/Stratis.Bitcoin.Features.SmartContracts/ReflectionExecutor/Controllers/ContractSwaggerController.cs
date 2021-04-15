200823fc46f0ffe1f0274af75fde736d968dae54
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-15 18:08:27 +1000

    Add contract swagger controller implementation

--- src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/ContractSwaggerController.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/ContractSwaggerController.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
 using NBitcoin;
+using Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Swagger;
 using Stratis.Bitcoin.Features.Wallet.Interfaces;
+using Stratis.SmartContracts.CLR;
 using Stratis.SmartContracts.CLR.Loader;
 using Stratis.SmartContracts.Core.State;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
 {
@@ -15,19 +23,22 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
     /// Controller for dynamically generating swagger documents for smart contract assemblies.
     /// </summary>
     [Route("swagger/contracts")]
-    public class ContractSwaggerController
+    public class ContractSwaggerController : Controller
     {
+        private readonly SwaggerGeneratorOptions options;
         private readonly ILoader loader;
         private readonly IWalletManager walletmanager;
         private readonly IStateRepositoryRoot stateRepository;
         private readonly Network network;
 
         public ContractSwaggerController(
+            SwaggerGeneratorOptions options,
             ILoader loader,
             IWalletManager walletmanager,
             IStateRepositoryRoot stateRepository,
             Network network)
         {
+            this.options = options;
             this.loader = loader;
             this.walletmanager = walletmanager;
             this.stateRepository = stateRepository;
@@ -44,7 +55,33 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
         [HttpGet]
         public async Task<IActionResult> ContractSwaggerDoc(string address)
         {
-            return new BadRequestResult();
+            var code = this.stateRepository.GetCode(address.ToUint160(this.network));
+
+            if (code == null)
+                throw new Exception("Contract does not exist");
+
+            Result<IContractAssembly> assemblyLoadResult = this.loader.Load((ContractByteCode)code);
+
+            if (assemblyLoadResult.IsFailure)
+                throw new Exception("Error loading assembly");
+
+            IContractAssembly assembly = assemblyLoadResult.Value;
+
+            // Default wallet is the first wallet as ordered by name.
+            string defaultWalletName = this.walletmanager.GetWalletsNames().OrderBy(n => n).First();
+
+            // Default address is the first address with a balance, or string.Empty if no addresses have been created.
+            // Ordering this way is consistent with the wallet UI, ie. whatever appears first in the wallet will appear first here.
+            string defaultAddress = this.walletmanager.GetAccountAddressesWithBalance(defaultWalletName).FirstOrDefault()?.Address ?? string.Empty;
+
+            var swaggerGen = new ContractSwaggerDocGenerator(this.options, address, assembly, defaultWalletName, defaultAddress);
+
+            OpenApiDocument doc = swaggerGen.GetSwagger("contracts");
+
+            // TODO confirm v2/v3
+            var outputString = doc.Serialize(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Json);
+
+            return Ok(outputString);
         }
     }
 }
