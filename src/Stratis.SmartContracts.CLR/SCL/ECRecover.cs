aef637bb2d04895f2c963d41553241f4eed916bd
quantumagi <someguy.fromafrica@gmail.com>
2021-04-19 13:49:36 +1000

    [Infra] Modify signaturure verification method (#522)
    * Modify signaturure verification method
    
    * Changes based on feedback
    
    * Refactor

--- src/Stratis.SmartContracts.CLR/SCL/ECRecover.cs
+++ src/Stratis.SmartContracts.CLR/SCL/ECRecover.cs
@@ -9,6 +9,13 @@ namespace Stratis.SCL.Crypto
 {
     public static class ECRecover
     {
+        /// <summary>
+        /// Takes a message and signatures and recovers the list of addresses that produced the signatures.
+        /// </summary>
+        /// <param name="signatures">The list of signatures of the message.</param>
+        /// <param name="message">The message that was signed.</param>
+        /// <param name="addresses">The addresses returned are intersected with these addresses (if not <c>null</c>).</param>
+        /// <returns>The list of addresses that produced the signatures and constrained to the list provided in <paramref name="addresses"/>.</returns>
         private static Address[] VerifySignatures(string[] signatures, byte[] message, Address[] addresses)
         {
             try
@@ -37,17 +44,30 @@ namespace Stratis.SCL.Crypto
         /// <param name="addresses">The addresses returned are intersected with these addresses.</param>
         /// <param name="verifiedAddresses">The list of addresses that produced the signatures and constrained to the list provided in <paramref name="addresses"/>.</param>
         /// <returns>The boolean value returned only indicates whether the operation could be performed. The number of verified addresses should still be checked.</returns>
-        public static bool TryVerifySignatures(string[] signatures, byte[] message, Address[] addresses, out Address[] verifiedAddresses)
+        public static bool TryGetVerifiedSignatures(string[] signatures, byte[] message, Address[] addresses, out Address[] verifiedAddresses)
         {
             if (addresses == null)
             {
                 verifiedAddresses = null;
                 return false;
-            }      
+            }
 
             verifiedAddresses = VerifySignatures(signatures, message, addresses);
 
             return verifiedAddresses != null;
+        }
+
+        /// <summary>
+        /// Takes a message and signatures and recovers the list of addresses that produced the signatures.
+        /// </summary>
+        /// <param name="signatures">The list of signatures of the message.</param>
+        /// <param name="message">The message that was signed.</param>
+        /// <param name="addresses">The addresses returned are intersected with these addresses.</param>
+        /// <param name="verifiedAddresses">The list of addresses that produced the signatures and constrained to the list provided in <paramref name="addresses"/>.</param>
+        /// <returns>The boolean value returned only indicates whether the operation could be performed. The number of verified addresses should still be checked.</returns>
+        public static bool TryGetVerifiedSignatures(string[] signatures, string message, Address[] addresses, out Address[] verifiedAddresses)
+        {
+            return TryGetVerifiedSignatures(signatures, System.Text.Encoding.ASCII.GetBytes(message), addresses, out verifiedAddresses);
         }
     }
 }
