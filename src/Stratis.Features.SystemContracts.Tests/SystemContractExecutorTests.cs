db4d7f819bbbc5bc03d14e20c69f5920d003b7af
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-26 19:12:20 +1000

    Add basic test

--- src/Stratis.Features.SystemContracts.Tests/SystemContractExecutorTests.cs
+++ src/Stratis.Features.SystemContracts.Tests/SystemContractExecutorTests.cs
@@ -1,3 +1,6 @@
+using Moq;
+using Stratis.SmartContracts.Core;
+using Stratis.SmartContracts.Core.State;
 using Xunit;
 
 namespace Stratis.Features.SystemContracts.Tests
@@ -8,6 +11,18 @@ namespace Stratis.Features.SystemContracts.Tests
         public void Test()
         {
 
+        }
+    }
+
+    public class SystemContractExecutorFactoryTests
+    {
+        [Fact]
+        public void Should_Return_Executor()
+        {
+            var factory = new TypeExecutorFactory();
+            IContractExecutor executor = factory.CreateExecutor(Mock.Of<IStateRepositoryRoot>(), Mock.Of<IContractTransactionContext>());
+
+            Assert.NotNull(executor);
         }
     }
 }
