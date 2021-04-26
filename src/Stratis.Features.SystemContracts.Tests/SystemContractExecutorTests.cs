020688271affd794b4b6025106910554ebba3e2a
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-26 19:41:40 +1000

    Executor constructor and test stub

---
+++ src/Stratis.Features.SystemContracts.Tests/SystemContractExecutorTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Stratis.Features.SystemContracts.Tests
+{
+    public class SystemContractExecutorTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
--- src/Stratis.Features.SystemContracts/SystemContractExecutor.cs
+++ src/Stratis.Features.SystemContracts/SystemContractExecutor.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using Stratis.SmartContracts.CLR;
-using Stratis.SmartContracts.CLR.ResultProcessors;
-using Stratis.SmartContracts.CLR.Serialization;
 using Stratis.SmartContracts.Core;
 using Stratis.SmartContracts.Core.State;
 
@@ -14,32 +12,18 @@ namespace Stratis.Features.SystemContracts
     public class SystemContractExecutor : IContractExecutor
     {
         private readonly IStateRepository stateRoot;
-        private readonly IContractRefundProcessor refundProcessor;
-        private readonly IContractTransferProcessor transferProcessor;
         private readonly ICallDataSerializer serializer;
-        private readonly IStateFactory stateFactory;
         private readonly IStateProcessor stateProcessor;
-        private readonly IContractPrimitiveSerializer contractPrimitiveSerializer;
 
         public SystemContractExecutor(
             ICallDataSerializer serializer,
             IStateRepository stateRoot,
-            IContractRefundProcessor refundProcessor,
-            IContractTransferProcessor transferProcessor,
-            IStateFactory stateFactory,
-            IStateProcessor stateProcessor,
-            IContractPrimitiveSerializer contractPrimitiveSerializer)
+            IStateProcessor stateProcessor)
         {
             this.stateRoot = stateRoot;
-            this.refundProcessor = refundProcessor;
-            this.transferProcessor = transferProcessor;
             this.serializer = serializer;
-            this.stateFactory = stateFactory;
             this.stateProcessor = stateProcessor;
-            this.contractPrimitiveSerializer = contractPrimitiveSerializer;
         }
-
-        public IContractTransferProcessor TransferProcessor { get; }
 
         public IContractExecutionResult Execute(IContractTransactionContext transactionContext)
         {
