73d2ccc203f0792d82e0a157ab387e1287f6fcd9
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-26 18:02:11 +1000

    Stub type executor factory

---
+++ src/Stratis.Features.SystemContracts/TypeExecutorFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Stratis.SmartContracts.Core;
+using Stratis.SmartContracts.Core.State;
+
+namespace Stratis.Features.SystemContracts
+{
+    /// <summary>
+    /// Factory for creating an executor that runs system contracts that are first-class types.
+    /// </summary>
+    public class TypeExecutorFactory : IContractExecutorFactory
+    {
+        public IContractExecutor CreateExecutor(IStateRepositoryRoot stateRepository, IContractTransactionContext transactionContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
