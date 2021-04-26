a5d307b0c8881556614c1f667f2d8f5f1bdf2817
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-26 18:02:54 +1000

    Stub SystemContractExecutor

---
+++ src/Stratis.Features.SystemContracts/SystemContractExecutor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Stratis.SmartContracts.Core;
+
+namespace Stratis.Features.SystemContracts
+{
+    public class SystemContractExecutor : IContractExecutor
+    {
+        public IContractExecutionResult Execute(IContractTransactionContext transactionContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
