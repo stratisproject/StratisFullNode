b6b45257349aaffae6b33eb186ad7422696b64d0
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-26 19:34:19 +1000

    Add tests again

---
+++ src/Stratis.Features.SystemContracts.Tests/SystemContractExecutorFactoryTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Moq;
+using Stratis.SmartContracts.Core;
+using Stratis.SmartContracts.Core.State;
+using Xunit;
+
+namespace Stratis.Features.SystemContracts.Tests
+{
+    public class SystemContractExecutorFactoryTests
+    {
+        [Fact]
+        public void Executor_Not_Null()
+        {
+            var factory = new TypeExecutorFactory();
+
+            Assert.NotNull(factory.CreateExecutor(Mock.Of<IStateRepositoryRoot>(), Mock.Of<IContractTransactionContext>()));
+        }
+    }
+}
