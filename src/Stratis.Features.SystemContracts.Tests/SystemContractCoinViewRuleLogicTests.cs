287a95ee7238d4bb88b7526e9ee614f40f6b30c1
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-26 19:54:31 +1000

    Begin adding SystemContractCoinviewRuleLogic

---
+++ src/Stratis.Features.SystemContracts.Tests/SystemContractCoinViewRuleLogicTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Stratis.Features.SystemContracts.Tests
+{
+    public class SystemContractCoinViewRuleLogicTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ src/Stratis.Features.SystemContracts/SystemContractCoinViewRuleLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBitcoin;
+using Stratis.Bitcoin.Base.Deployments;
+using Stratis.Bitcoin.Consensus.Rules;
+using Stratis.Bitcoin.Features.Consensus.Interfaces;
+
+namespace Stratis.Features.SystemContracts
+{
+    public class SystemContractCoinViewRuleLogic : ISmartContractCoinViewRuleLogic
+    {
+        public bool CheckInput(Func<Transaction, int, TxOut, PrecomputedTransactionData, TxIn, DeploymentFlags, bool> baseCheckInput, Transaction tx, int inputIndexCopy, TxOut txout, PrecomputedTransactionData txData, TxIn input, DeploymentFlags flags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RunAsync(Func<RuleContext, Task> baseRunAsync, RuleContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCoinView(Action<RuleContext, Transaction> baseUpdateUTXOSet, RuleContext context, Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
