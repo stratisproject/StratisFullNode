27eede5f6dd642abff13515cd0644096d597b557
Rowan de Haas <rowandh@users.noreply.github.com>
2021-04-29 12:58:53 +1000

    Add missing pos block definition

---
+++ src/Stratis.Bitcoin.Features.SmartContracts/PoS/SmartContractPosBlockDefinition.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.Logging;
+using NBitcoin;
+using Stratis.Bitcoin.Base.Deployments;
+using Stratis.Bitcoin.Consensus;
+using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.Consensus.CoinViews;
+using Stratis.Bitcoin.Features.Consensus.Interfaces;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.MemoryPool.Interfaces;
+using Stratis.Bitcoin.Features.Miner;
+using Stratis.Bitcoin.Features.SmartContracts.Caching;
+using Stratis.Bitcoin.Features.SmartContracts.PoW;
+using Stratis.Bitcoin.Mining;
+using Stratis.Bitcoin.Utilities;
+using Stratis.SmartContracts.CLR;
+using Stratis.SmartContracts.Core;
+using Stratis.SmartContracts.Core.State;
+using Stratis.SmartContracts.Core.Util;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.PoS
+{
+    /// <summary>
+    /// Derives from <see cref="SmartContractBlockDefinition"/>. Amends a few things for PoS.
+    /// </summary>
+    public class SmartContractPosBlockDefinition : SmartContractBlockDefinition
+    {
+        /// <summary>Database of stake related data for the current blockchain.</summary>
+        private readonly IStakeChain stakeChain;
+
+        /// <summary>Provides functionality for checking validity of PoS blocks.</summary>
+        private readonly IStakeValidator stakeValidator;
+
+        public SmartContractPosBlockDefinition(
+            IBlockBufferGenerator blockBufferGenerator,
+            ICoinView coinView,
+            IConsensusManager consensusManager,
+            IDateTimeProvider dateTimeProvider,
+            IContractExecutorFactory executorFactory,
+            ILoggerFactory loggerFactory,
+            ITxMempool mempool,
+            MempoolSchedulerLock mempoolLock,
+            Network network,
+            ISenderRetriever senderRetriever,
+            IStateRepositoryRoot stateRoot,
+            IBlockExecutionResultCache executionCache,
+            ICallDataSerializer callDataSerializer,
+            MinerSettings minerSettings,
+            IStakeChain stakeChain,
+            IStakeValidator stakeValidator,
+            NodeDeployments nodeDeployments)
+            : base(blockBufferGenerator, coinView, consensusManager, dateTimeProvider, executorFactory, loggerFactory, mempool,
+                mempoolLock, minerSettings, network, senderRetriever, stateRoot, executionCache, callDataSerializer, nodeDeployments)
+        {
+            // TODO: Fix gross MinerSettings injection ^^
+
+            this.stakeChain = stakeChain;
+            this.stakeValidator = stakeValidator;
+        }
+
+        /// <inheritdoc/>
+        public override BlockTemplate Build(ChainedHeader chainTip, Script scriptPubKey)
+        {
+            base.BuildNoCache(chainTip, scriptPubKey);
+
+            // No PoW reward when staking.
+            this.coinbase.Outputs[0].ScriptPubKey = new Script();
+            this.coinbase.Outputs[0].Value = Money.Zero;
+
+            // Cache the results. We don't need to execute these again when validating.
+            var cacheModel = new BlockExecutionResultModel(this.stateSnapshot, this.receipts);
+            this.executionCache.StoreExecutionResult(this.BlockTemplate.Block.GetHash(), cacheModel);
+
+            return this.BlockTemplate;
+        }
+
+        /// <inheritdoc/>
+        public override void UpdateHeaders()
+        {
+            base.UpdateHeaders();
+
+            this.block.Header.Bits = this.stakeValidator.GetNextTargetRequired(this.stakeChain, this.ChainTip, this.Network.Consensus, true);
+        }
+
+        /// <inheritdoc/>
+        protected override void ComputeBlockVersion()
+        {
+            base.ComputeBlockVersion();
+            this.block.Header.Version |= PosBlockHeader.ExtendedHeaderBit;
+        }
+    }
+}
