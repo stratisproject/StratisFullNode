7aaa6bf0ff813ca3c0a4d1dadba2350c7d248714
Francois de la Rouviere <fassadlr@gmail.com>
2021-05-10 08:09:28 +0100

    busy with test

--- src/Stratis.Bitcoin.IntegrationTests/Wallet/WalletHistoryIntegrationTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/Wallet/WalletHistoryIntegrationTests.cs
@@ -49,7 +49,7 @@ namespace Stratis.Bitcoin.IntegrationTests.Wallet
             // Mine some blocks to receive the premine.
             TestHelper.MineBlocks(nodeA, 5, miningAddress: miningAddresses.First());
 
-            // Test some coins in various inputs to nodeB
+            // Send some coins to nodeB
             TestHelper.SendCoins(nodeA, nodeB, Money.Coins(1000));
 
             // Mine the coins and advance the chain.
@@ -80,35 +80,76 @@ namespace Stratis.Bitcoin.IntegrationTests.Wallet
                                 .GetAsync()
                                 .ReceiveJson<WalletHistoryModel>().GetAwaiter().GetResult();
 
+            // Assert ordering
+            var history = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.ToList();
+
+            // Staking items should appear first in the result.
+            var index = 0;
+            do
+            {
+                var item = history[index];
+                if (item.Type == TransactionItemType.Mined)
+                    break;
+
+                Assert.Equal(Money.Coins(9), item.Amount);
+                Assert.Equal(TransactionItemType.Staked, item.Type);
+
+                index += 1;
+            } while (true);
+
+            // Then 15 + 1 mining txs (the 1 is for mining the send tx)
+            for (int mIndex = index; mIndex < index + 16; mIndex++)
+            {
+                var item = history[mIndex];
+                Assert.Equal(Money.Coins(18), item.Amount);
+                Assert.Equal(miningAddresses.First(), item.ToAddress);
+                Assert.Equal(TransactionItemType.Mined, item.Type);
+            }
+
+            // Then a send
+            var send = history[index + 16 + 1];
+            Assert.Equal(Money.Coins(1000) + send.Fee, send.Amount);
+            Assert.Equal(TransactionItemType.Send, send.Type);
+
+            // Then 5 mining txs
+            for (int mIndex = index + 17; mIndex < mIndex + 17 + 5; mIndex++)
+            {
+                var item = history[mIndex];
+                Assert.Equal(Money.Coins(18), item.Amount);
+                Assert.Equal(miningAddresses.First(), item.ToAddress);
+                Assert.Equal(TransactionItemType.Mined, item.Type);
+            }
+
             // Assert mining transactions
-            var miningTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Mined);
-            Assert.Equal(21, miningTxs.Count());
-            var miningTx = miningTxs.First();
-            Assert.Equal(Money.Coins(18), miningTx.Amount);
-            Assert.Equal(miningAddresses.First(), miningTx.ToAddress);
-            Assert.Equal(TransactionItemType.Mined, miningTx.Type);
+            //var miningTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Mined);
+            //Assert.Equal(21, miningTxs.Count());
+            //var miningTx = miningTxs.First();
+            //Assert.Equal(Money.Coins(18), miningTx.Amount);
+            //Assert.Equal(miningAddresses.First(), miningTx.ToAddress);
+            //Assert.Equal(TransactionItemType.Mined, miningTx.Type);
 
-            // Assert staking transactions
-            var stakingTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Staked);
-            Assert.True(stakingTxs.Count() >= 9); // This is because StopStake could on occasion not stop the staking process quick enough.
-            var stakingTx = stakingTxs.First();
-            Assert.Equal(Money.Coins(9), stakingTx.Amount);
-            Assert.Equal(TransactionItemType.Staked, stakingTx.Type);
+            //// Assert staking transactions
+            //var stakingTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Staked);
+            //Assert.True(stakingTxs.Count() >= 9); // This is because StopStake could on occasion not stop the staking process quick enough.
+            //var stakingTx = stakingTxs.First();
+            //Assert.Equal(Money.Coins(9), stakingTx.Amount);
+            //Assert.Equal(TransactionItemType.Staked, stakingTx.Type);
 
-            // Assert send transaction
-            var sendTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Send);
-            Assert.Single(sendTxs);
-            var sendTx = sendTxs.First();
-            Assert.Equal(Money.Coins(1000) + sendTx.Fee, sendTx.Amount);
-            Assert.Equal(TransactionItemType.Send, sendTx.Type);
+            //// Assert send transaction
+            //var sendTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Send);
+            //Assert.Single(sendTxs);
+            //var sendTx = sendTxs.First();
+            //Assert.Equal(Money.Coins(1000) + sendTx.Fee, sendTx.Amount);
+            //Assert.Equal(TransactionItemType.Send, sendTx.Type);
 
             // Assert payment details and change on sent tx.
+            var sendTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Send);
+            var sendTx = sendTxs.First();
             var singleTxCall = $"http://localhost:{nodeA.ApiPort}/api"
                 .AppendPathSegment("wallet/history")
                 .SetQueryParams(new WalletHistoryRequest { WalletName = "mywallet", AccountName = "account 0", SearchQuery = sendTx.Id.ToString() })
                 .GetAsync()
                 .ReceiveJson<WalletHistoryModel>().GetAwaiter().GetResult();
-            sendTx = singleTxCall.AccountsHistoryModel.First().TransactionsHistory.First();
             Assert.Equal(2, sendTx.Payments.Count);
             var changePayments = sendTx.Payments.Where(p => p.IsChange);
             var normalPayments = sendTx.Payments.Where(p => !p.IsChange);
@@ -117,7 +158,7 @@ namespace Stratis.Bitcoin.IntegrationTests.Wallet
             Assert.Equal(Money.Coins(1000), normalPayments.First().Amount);
 
             // Assert Pagination and ordering
-            historyNodeA = $"http://localhost:{nodeA.ApiPort}/api"
+            var paging = $"http://localhost:{nodeA.ApiPort}/api"
                 .AppendPathSegment("wallet/history")
                 .SetQueryParams(new WalletHistoryRequest { WalletName = "mywallet", AccountName = "account 0", Skip = 0, Take = 10 })
                 .GetAsync()
@@ -125,8 +166,8 @@ namespace Stratis.Bitcoin.IntegrationTests.Wallet
             Assert.Equal(10, historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Count());
 
             // There should be no sends in this set
-            sendTxs = historyNodeA.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Send);
-            Assert.Empty(sendTxs);
+            var pagedSends = paging.AccountsHistoryModel.First().TransactionsHistory.Where(t => t.Type == TransactionItemType.Send);
+            Assert.Empty(pagedSends);
 
             // Call the history method on node B
             WalletHistoryModel historyNodeB = $"http://localhost:{nodeB.ApiPort}/api"
