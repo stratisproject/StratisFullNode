f1cbff6d13c53d05d0f30bff4e06458c32e2a8c4
Francois de la Rouviere <fassadlr@gmail.com>
2021-05-28 12:21:19 +0100

    Fix Build

--- src/Stratis.Features.FederatedPeg/SourceChain/DepositExtractor.cs
+++ src/Stratis.Features.FederatedPeg/SourceChain/DepositExtractor.cs
@@ -5,7 +5,6 @@ using System.Net.Http;
 using NBitcoin;
 using NLog;
 using Stratis.Bitcoin.Features.ExternalApi;
-using Stratis.Features.Collateral.CounterChain;
 using Stratis.Features.FederatedPeg.Interfaces;
 using Stratis.Features.PoA.Collateral.CounterChain;
 
@@ -46,27 +45,27 @@ namespace Stratis.Features.FederatedPeg.SourceChain
             { "CirrusRegTest", new List<IDeposit> {
                 new Deposit(
                     0x1 /* Tx of deposit being redone */, DepositRetrievalType.Small, new Money(10, MoneyUnit.BTC),
-                    "qZc3WCqj8dipxUau1q18rT6EMBN6LRZ44A", DestinationChain.STRAX, 85, 0)
+                    "qZc3WCqj8dipxUau1q18rT6EMBN6LRZ44A", 85, 0)
                 }
             },
 
             { "CirrusTest", new List<IDeposit> {
                 new Deposit(
                     uint256.Parse("7691bf9838ebdede6db0cb466d93c1941d13894536dc3a5db8289ad04b28d12c"), DepositRetrievalType.Small, new Money(20, MoneyUnit.BTC),
-                    "qeyK7poxBE1wy8H24a7AcpLySCsfiqAo6A", DestinationChain.STRAX, 2_177_900, 0)
+                    "qeyK7poxBE1wy8H24a7AcpLySCsfiqAo6A", 2_177_900, 0)
                 }
             },
 
             { "CirrusMain", new List<IDeposit> {
                 new Deposit(
                     uint256.Parse("6179ee3332348948641210e2c9358a41aa8aaf2924d783e52bc4cec47deef495") /* Tx of deposit being redone */, DepositRetrievalType.Normal,
-                    new Money(239, MoneyUnit.BTC), "XNrgftud4ExFL7dXEHjPPx3JX22qvFy39v", DestinationChain.STRAX, 2500000, 0),
+                    new Money(239, MoneyUnit.BTC), "XNrgftud4ExFL7dXEHjPPx3JX22qvFy39v", 2500000, 0),
                 new Deposit(
                     uint256.Parse("b8417bdbe7690609b2fff47d6d8b39bed69993df6656d7e10197c141bdacdd90") /* Tx of deposit being redone */, DepositRetrievalType.Normal,
-                    new Money(640, MoneyUnit.BTC), "XCDeD5URnSbExLsuVYu7yufpbmb6KvaX8D", DestinationChain.STRAX, 2500000, 0),
+                    new Money(640, MoneyUnit.BTC), "XCDeD5URnSbExLsuVYu7yufpbmb6KvaX8D", 2500000, 0),
                 new Deposit(
                     uint256.Parse("375ed4f028d3ef32397641a0e307e1705fc3f8ba76fa776ce33fff53f52b0e1c") /* Tx of deposit being redone */, DepositRetrievalType.Large,
-                    new Money(1649, MoneyUnit.BTC), "XCrHzx5AUnNChCj8MP9j82X3Y7gLAtULtj", DestinationChain.STRAX, 2500000, 0),
+                    new Money(1649, MoneyUnit.BTC), "XCrHzx5AUnNChCj8MP9j82X3Y7gLAtULtj", 2500000, 0),
                 }
             }
         };
@@ -118,7 +117,7 @@ namespace Stratis.Features.FederatedPeg.SourceChain
                 return null;
 
             var depositsToMultisig = transaction.Outputs.Where(output =>
-                output.ScriptPubKey == this.depositScript &&
+                output.ScriptPubKey == this.federatedPegSettings.MultiSigRedeemScript.PaymentScript &&
                 output.Value >= FederatedPegSettings.CrossChainTransferMinimum).ToList();
 
             if (!depositsToMultisig.Any())
@@ -132,7 +131,7 @@ namespace Stratis.Features.FederatedPeg.SourceChain
                 {
                     return null;
                 }
-                
+
                 conversionTransaction = true;
             }
 
