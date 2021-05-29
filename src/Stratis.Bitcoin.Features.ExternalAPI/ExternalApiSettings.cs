5ff666dbe92b506041f245a1faafec1a5cec5027
Kevin Loubser <zeptin@gmail.com>
2021-05-29 11:27:38 +0200

    Add Etherscan API key setting

--- src/Stratis.Bitcoin.Features.ExternalAPI/ExternalApiSettings.cs
+++ src/Stratis.Bitcoin.Features.ExternalAPI/ExternalApiSettings.cs
@@ -4,6 +4,8 @@ namespace Stratis.Bitcoin.Features.ExternalApi
 {
     public class ExternalApiSettings
     {
+        public const string EtherscanApiKeyKey = "etherscanapikey";
+
         public const string EtherscanGasOracleUrlKey = "etherscangasoracle";
 
         public const string EthereumGasPriceTrackingKey = "ethereumgaspricetracking";
@@ -11,6 +13,8 @@ namespace Stratis.Bitcoin.Features.ExternalApi
         public const string PriceUrlKey = "ethereumpriceurl";
 
         public const string PriceTrackingKey = "pricetracking";
+
+        public string EtherscanApiKey { get; set; }
 
         public string EtherscanGasOracleUrl { get; set; }
 
@@ -22,7 +26,9 @@ namespace Stratis.Bitcoin.Features.ExternalApi
 
         public ExternalApiSettings(NodeSettings nodeSettings)
         {
-            this.EtherscanGasOracleUrl = nodeSettings.ConfigReader.GetOrDefault(EtherscanGasOracleUrlKey, "https://api.etherscan.io/api?module=gastracker&action=gasoracle");
+            // To avoid any rate limiting by Etherscan it is better to have an API key defined, but the API is still supposed to work to a limited extent without one.
+            this.EtherscanApiKey = nodeSettings.ConfigReader.GetOrDefault(EtherscanApiKeyKey, "YourApiKeyToken");
+            this.EtherscanGasOracleUrl = nodeSettings.ConfigReader.GetOrDefault(EtherscanGasOracleUrlKey, "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=" + this.EtherscanApiKey);
             this.EthereumGasPriceTracking = nodeSettings.ConfigReader.GetOrDefault(EthereumGasPriceTrackingKey, true);
             this.PriceUrl = nodeSettings.ConfigReader.GetOrDefault(PriceUrlKey, "https://api.coingecko.com/api/v3/simple/price?ids=stratis,ethereum&vs_currencies=usd");
             this.PriceTracking = nodeSettings.ConfigReader.GetOrDefault(PriceTrackingKey, true);
