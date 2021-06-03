45e9a3e9938dd4711ff744dc19bd9aed013a33c9
noescape0 <NoEscape0@ya.ru>
2021-06-03 16:30:22 +0300

    Unity3d API feature (#569)
    * Added unity3d api feature
    
    * disable other controllers on unity api
    
    * add methods to unity api controller
    
    * cleanup
    
    * Cleanup and added api method
    
    * more cleanup
    
    * provide UTXOs for address
    
    * added sat value to utxos output
    
    * add sendtx endpoint
    
    * changed api return types for nswagger
    
    * upd controller
    
    * cleanup
    
    * added api method
    
    * fix per review
    
    * Update src/Stratis.Features.Unity3dApi/Controllers/Unity3dController.cs
    
    Co-authored-by: zeptin <zeptin@gmail.com>
    
    * fixes per review
    
    Co-authored-by: zeptin <zeptin@gmail.com>

---
+++ src/Stratis.Features.Unity3dApi/ResponseModels.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using NBitcoin;
+
+namespace Stratis.Features.Unity3dApi
+{
+    public class GetUTXOsResponseModel
+    {
+        public long BalanceSat;
+
+        public List<UTXOModel> UTXOs;
+
+        public string Reason;
+    }
+
+    public class UTXOModel
+    {
+        public UTXOModel()
+        {
+        }
+
+        public UTXOModel(OutPoint outPoint, Money value)
+        {
+            this.Hash = outPoint.Hash.ToString();
+            this.N = outPoint.N;
+            this.Satoshis = value.Satoshi;
+        }
+
+        public string Hash;
+
+        public uint N;
+
+        public long Satoshis;
+    }
+
+    public sealed class TipModel
+    {
+        public string TipHash { get; set; }
+
+        public int TipHeight { get; set; }
+    }
+
+    public sealed class RawTxModel
+    {
+        public string Hex { get; set; }
+    }
+}
