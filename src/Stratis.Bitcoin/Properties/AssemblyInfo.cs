31ce361681ef2bd6b096e6a7969eb3c3b904f30f
Francois de la Rouviere <fassadlr@gmail.com>
2021-06-16 16:06:19 +0100

    Bump Version

--- src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
+++ src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.15")]
-[assembly: AssemblyFileVersion("1.0.8.15")]
+[assembly: AssemblyVersion("1.0.8.16")]
+[assembly: AssemblyFileVersion("1.0.8.16")]
 [assembly: InternalsVisibleTo("Stratis.Bitcoin.Tests")]
