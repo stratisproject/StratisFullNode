14b7fc5a933fd619349de5262ee2cdb56f4eef50
quantumagi <someguy.fromafrica@gmail.com>
2021-06-17 19:59:27 +1000

    Revert tests

--- src/Stratis.Bitcoin.Features.PoA.Tests/Rules/PoAHeaderSignatureRuleTests.cs
+++ src/Stratis.Bitcoin.Features.PoA.Tests/Rules/PoAHeaderSignatureRuleTests.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Moq;
 using NBitcoin;
 using Stratis.Bitcoin.Configuration;
 using Stratis.Bitcoin.Consensus;
 using Stratis.Bitcoin.Consensus.Rules;
 using Stratis.Bitcoin.Features.PoA.BasePoAFeatureConsensusRules;
-using Stratis.Bitcoin.Interfaces;
 using Xunit;
 
 namespace Stratis.Bitcoin.Features.PoA.Tests.Rules
