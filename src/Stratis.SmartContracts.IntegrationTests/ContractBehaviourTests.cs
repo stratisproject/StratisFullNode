5823239f2249b93b48225a16a0311ba47549602a
Rowan de Haas <rowandh@users.noreply.github.com>
2021-06-28 11:35:52 +1000

    Add tests demonstrating issue

--- src/Stratis.SmartContracts.IntegrationTests/ContractBehaviourTests.cs
+++ src/Stratis.SmartContracts.IntegrationTests/ContractBehaviourTests.cs
@@ -130,6 +130,105 @@ namespace Stratis.SmartContracts.IntegrationTests
             Assert.True(receipt.Success);
         }
 
+        /// <summary>
+        /// Tests that when clearing persistent state the data is empty immediately after.
+        /// </summary>
+        [Fact]
+        public void PersistentState_Clear_State_Should_Be_Empty_In_Same_Tx()
+        {            
+            // Ensure fixture is funded.
+            this.mockChain.MineBlocks(1);
+
+            // Deploy contract
+            ContractCompilationResult compilationResult = ContractCompiler.CompileFile("SmartContracts/ClearDataContract.cs");
+
+            Assert.True(compilationResult.Success);
+            BuildCreateContractTransactionResponse preResponse = this.node1.SendCreateContractTransaction(compilationResult.Compilation, 0);
+            this.mockChain.WaitAllMempoolCount(1);
+            this.mockChain.MineBlocks(1);
+            Assert.NotNull(this.node1.GetCode(preResponse.NewContractAddress));
+
+            uint256 currentHash = this.node1.GetLastBlock().GetHash();
+
+            // Clear the data and check that it's empty
+            BuildCallContractTransactionResponse response = this.node1.SendCallContractTransaction(
+                nameof(ClearDataContract.ClearDataAndCheck),
+                preResponse.NewContractAddress,
+                0);
+            this.mockChain.WaitAllMempoolCount(1);
+            this.mockChain.MineBlocks(1);
+
+            NBitcoin.Block lastBlock = this.node1.GetLastBlock();
+
+            // Blocks progressed
+            Assert.NotEqual(currentHash, lastBlock.GetHash());
+
+            ReceiptResponse receipt = this.node1.GetReceipt(response.TransactionId.ToString());
+            Assert.True(receipt.Success);
+            Assert.Equal(true.ToString(), receipt.ReturnValue);
+        }
+
+        /// <summary>
+        /// Tests that when clearing persistent state the data is empty when it is accessed in a new transaction.
+        /// 
+        /// The distinction is important because between txes the storage cache is flushed and values are pushed to
+        /// the underlying storage layer.
+        /// </summary>
+        [Fact]
+        public void PersistentState_Clear_State_Should_Be_Empty_In_New_Tx()
+        {
+            // Ensure fixture is funded.
+            this.mockChain.MineBlocks(1);
+
+            // Deploy contract
+            ContractCompilationResult compilationResult = ContractCompiler.CompileFile("SmartContracts/ClearDataContract.cs");
+
+            Assert.True(compilationResult.Success);
+            BuildCreateContractTransactionResponse preResponse = this.node1.SendCreateContractTransaction(compilationResult.Compilation, 0);
+            this.mockChain.WaitAllMempoolCount(1);
+            this.mockChain.MineBlocks(1);
+            Assert.NotNull(this.node1.GetCode(preResponse.NewContractAddress));
+
+            uint256 currentHash = this.node1.GetLastBlock().GetHash();
+
+            // Clear the data and check that it's empty
+            BuildCallContractTransactionResponse response = this.node1.SendCallContractTransaction(
+                nameof(ClearDataContract.ClearData),
+                preResponse.NewContractAddress,
+                0);
+            this.mockChain.WaitAllMempoolCount(1);
+            this.mockChain.MineBlocks(1);
+
+            NBitcoin.Block lastBlock = this.node1.GetLastBlock();
+
+            // Blocks progressed
+            Assert.NotEqual(currentHash, lastBlock.GetHash());
+
+            ReceiptResponse receipt = this.node1.GetReceipt(response.TransactionId.ToString());
+
+            // Invoke Check in a new tx. It should succeed
+            response = this.node1.SendCallContractTransaction(
+               nameof(ClearDataContract.Check),
+               preResponse.NewContractAddress,
+               0);
+            this.mockChain.WaitAllMempoolCount(1);
+            this.mockChain.MineBlocks(1);
+
+            lastBlock = this.node1.GetLastBlock();
+
+            // Blocks progressed
+            Assert.NotEqual(currentHash, lastBlock.GetHash());
+                        
+            receipt = this.node1.GetReceipt(response.TransactionId.ToString());
+            Assert.True(receipt.Success);
+
+            // The storage value should be null because the transaction completed successfully.
+            Assert.Null(this.node1.GetStorageValue(preResponse.NewContractAddress, nameof(ClearDataContract.Data)));
+
+            // Check should be true because the data is now cleared.
+            Assert.Equal(true.ToString(), receipt.ReturnValue);
+        }
+
         [Fact]
         public void Local_Call_At_Height()
         {
