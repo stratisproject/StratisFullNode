a6cd90b45f9ecafe6280eb0977c15e121125a0da
Francois de la Rouviere <fassadlr@gmail.com>
2021-06-29 16:34:06 +0100

    Remove unnecessary logs

--- src/Stratis.Features.FederatedPeg/Conversion/ConversionRequestRepository.cs
+++ src/Stratis.Features.FederatedPeg/Conversion/ConversionRequestRepository.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using NLog;
 
 namespace Stratis.Features.FederatedPeg.Conversion
 {
@@ -34,44 +33,32 @@ namespace Stratis.Features.FederatedPeg.Conversion
     {
         private IConversionRequestKeyValueStore KeyValueStore { get; }
 
-        private readonly ILogger logger;
-
         public ConversionRequestRepository(IConversionRequestKeyValueStore conversionRequestKeyValueStore)
         {
             this.KeyValueStore = conversionRequestKeyValueStore;
-
-            this.logger = LogManager.GetCurrentClassLogger();
         }
 
         /// <inheritdoc />
         public void Save(ConversionRequest request)
         {
-            this.logger.Debug($"Saving conversion request {request.RequestId} to store.");
-
             this.KeyValueStore.SaveValue(request.RequestId, request);
         }
 
         /// <inheritdoc />
         public ConversionRequest Get(string requestId)
         {
-            this.logger.Debug($"Retrieving conversion request {requestId} from store.");
-
             return this.KeyValueStore.LoadValue<ConversionRequest>(requestId);
         }
 
         /// <inheritdoc />
         public List<ConversionRequest> GetAllMint(bool onlyUnprocessed)
         {
-            this.logger.Debug($"Retrieving all mint requests from store, {nameof(onlyUnprocessed)}={onlyUnprocessed}");
-
             return this.KeyValueStore.GetAll(ConversionRequestType.Mint, onlyUnprocessed);
         }
 
         /// <inheritdoc />
         public List<ConversionRequest> GetAllBurn(bool onlyUnprocessed)
         {
-            this.logger.Debug($"Retrieving all burn requests from store, {nameof(onlyUnprocessed)}={onlyUnprocessed}");
-
             return this.KeyValueStore.GetAll(ConversionRequestType.Burn, onlyUnprocessed);
         }
 
