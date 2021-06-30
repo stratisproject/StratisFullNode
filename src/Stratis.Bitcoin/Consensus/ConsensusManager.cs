aa9b4bbfa58788afc7c20f5e79ed351f347ac0d5
quantumagi <someguy.fromafrica@gmail.com>
2021-06-30 17:10:38 +1000

    Add "OnBlockConnected" signals bench to console (#588)
    * Add "OnBlockConnected" signals bench to console
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Remove whitespace
    
    * Remove unused references
    
    * Refactor
    
    * Add rounding

--- src/Stratis.Bitcoin/Consensus/ConsensusManager.cs
+++ src/Stratis.Bitcoin/Consensus/ConsensusManager.cs
@@ -14,6 +14,7 @@ using Stratis.Bitcoin.Connection;
 using Stratis.Bitcoin.Consensus.PerformanceCounters.ConsensusManager;
 using Stratis.Bitcoin.Consensus.ValidationResults;
 using Stratis.Bitcoin.Consensus.Validators;
+using Stratis.Bitcoin.EventBus;
 using Stratis.Bitcoin.EventBus.CoreEvents;
 using Stratis.Bitcoin.Interfaces;
 using Stratis.Bitcoin.P2P.Peer;
@@ -1481,7 +1482,9 @@ namespace Stratis.Bitcoin.Consensus
         [NoTrace]
         private void AddBenchStats(StringBuilder benchLog)
         {
-            benchLog.AppendLine(this.performanceCounter.TakeSnapshot().ToString());
+            benchLog.Append(this.performanceCounter.TakeSnapshot().ToString());
+            benchLog.AppendLine();
+            benchLog.AppendLine(((InMemoryEventBus)this.signals).GetPerformanceCounter().TakeSnapshot().GetEventStats(typeof(BlockConnected)));
         }
 
         [NoTrace]
