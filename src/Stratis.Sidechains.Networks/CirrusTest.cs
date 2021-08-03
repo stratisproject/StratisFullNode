33eef74a4897a8db1d2f1cde3074632ddf3d212e
quantumagi <someguy.fromafrica@gmail.com>
2021-08-03 18:58:47 +1000

    Update CirrusTest activation height

--- src/Stratis.Sidechains.Networks/CirrusTest.cs
+++ src/Stratis.Sidechains.Networks/CirrusTest.cs
@@ -126,7 +126,7 @@ namespace Stratis.Sidechains.Networks
                 EnforceMinProtocolVersionAtBlockHeight = 505900, // setting the value to zero makes the functionality inactive
                 EnforcedMinProtocolVersion = ProtocolVersion.CIRRUS_VERSION, // minimum protocol version which will be enforced at block height defined in EnforceMinProtocolVersionAtBlockHeight
                 VotingManagerV2ActivationHeight = 1_999_500,
-                GetMiningTimestampV2ActivationHeight = 5_000_000
+                GetMiningTimestampV2ActivationHeight = 2_600_000
             };
 
             var buriedDeployments = new BuriedDeploymentsArray
