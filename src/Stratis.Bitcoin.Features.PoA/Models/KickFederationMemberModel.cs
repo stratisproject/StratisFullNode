d23494f0ca695be59eea3265ae8d7e79da5ec6b9
Francois de la Rouviere <fassadlr@gmail.com>
2021-09-15 12:26:31 +0100

    Add kick member API endpoint

---
+++ src/Stratis.Bitcoin.Features.PoA/Models/KickFederationMemberModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Stratis.Bitcoin.Features.PoA.Models
+{
+    public sealed class KickFederationMemberModel
+    {
+        [JsonProperty("pubkey")]
+        public string PubKey { get; set; }
+    }
+}
