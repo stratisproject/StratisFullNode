20cff7a6c89f6d2d28f6f2b68acf1ec5e2238a8b
Francois de la Rouviere <fassadlr@gmail.com>
2021-09-27 11:44:37 +0100

    Add IBD check

--- src/Stratis.Bitcoin.Features.PoA/Voting/JoinFederationRequestService.cs
+++ src/Stratis.Bitcoin.Features.PoA/Voting/JoinFederationRequestService.cs
@@ -13,6 +13,7 @@ using Stratis.Bitcoin.Features.Wallet.Controllers;
 using Stratis.Bitcoin.Features.Wallet.Interfaces;
 using Stratis.Bitcoin.Features.Wallet.Models;
 using Stratis.Bitcoin.Features.Wallet.Services;
+using Stratis.Bitcoin.Interfaces;
 using Stratis.Bitcoin.PoA.Features.Voting;
 using Stratis.Features.PoA.Collateral.CounterChain;
 
@@ -29,16 +30,18 @@ namespace Stratis.Features.PoA.Voting
         private readonly IFederationManager federationManager;
         private readonly IFullNode fullNode;
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly IInitialBlockDownloadState initialBlockDownloadState;
         private readonly ILoggerFactory loggerFactory;
         private readonly PoANetwork network;
         private readonly NodeSettings nodeSettings;
         private readonly VotingManager votingManager;
 
-        public JoinFederationRequestService(ICounterChainSettings counterChainSettings, IFederationManager federationManager, IFullNode fullNode, IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory, Network network, NodeSettings nodeSettings, VotingManager votingManager)
+        public JoinFederationRequestService(ICounterChainSettings counterChainSettings, IFederationManager federationManager, IFullNode fullNode, IInitialBlockDownloadState initialBlockDownloadState, IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory, Network network, NodeSettings nodeSettings, VotingManager votingManager)
         {
             this.counterChainSettings = counterChainSettings;
             this.federationManager = federationManager;
             this.fullNode = fullNode;
+            this.initialBlockDownloadState = initialBlockDownloadState;
             this.httpClientFactory = httpClientFactory;
             this.loggerFactory = loggerFactory;
             this.network = network as PoANetwork;
@@ -48,6 +51,10 @@ namespace Stratis.Features.PoA.Voting
 
         public async Task<PubKey> JoinFederationAsync(JoinFederationRequestModel request, CancellationToken cancellationToken)
         {
+            // Wait until the node is synced before joining.
+            if (this.initialBlockDownloadState.IsInitialBlockDownload())
+                throw new Exception($"Please wait until the node is synced with the network before attempting to join the federation.");
+
             // First ensure that this collateral address isnt already present in the federation.
             if (this.federationManager.GetFederationMembers().IsCollateralAddressRegistered(request.CollateralAddress))
                 throw new Exception($"The provided collateral address '{request.CollateralAddress}' is already present in the federation.");
