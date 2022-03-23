ba4d30341e54d00e7a88c29151800794aff360d9
Kevin Loubser <zeptin@gmail.com>
2022-03-23 10:29:24 +0200

    Added two RPC methods for listing transactions

--- src/Stratis.Bitcoin.IntegrationTests/API/ApiSteps.cs
+++ src/Stratis.Bitcoin.IntegrationTests/API/ApiSteps.cs
@@ -419,7 +419,7 @@ namespace Stratis.Bitcoin.IntegrationTests.API
         {
             var commands = JsonDataSerializer.Instance.Deserialize<List<RpcCommandModel>>(this.responseText);
 
-            commands.Count.Should().Be(37);
+            commands.Count.Should().Be(39);
         }
 
         private void status_information_is_returned()
