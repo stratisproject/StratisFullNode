88214a1862f82aeec91605bb89c62adae52e085e
Francois de la Rouviere <fassadlr@gmail.com>
2022-03-25 12:19:11 +0000

    Self review

--- src/Stratis.Features.FederatedPeg/Distribution/IRewardDistributionManager.cs
+++ src/Stratis.Features.FederatedPeg/Distribution/IRewardDistributionManager.cs
@@ -7,10 +7,10 @@ namespace Stratis.Features.FederatedPeg.Distribution
     public interface IRewardDistributionManager
     {
         /// <summary>
-        /// For wSTRAX and SRC20 to ERC20 transfers, the multisig needs to have a fee for submitting and confirming the transaction on the external chain, paid out ot them.
+        /// For wSTRAX and SRC20 to ERC20 transfers, the multisig needs to have a fee for submitting and confirming the transaction on the external chain, paid out to them.
         /// </summary>
         /// <param name="depositId">The id of the transaction/deposit on Cirrus.</param>
-        /// <param name="totalReward">The total fee that willl be distributed to the applicable multisig nodes.</param>
+        /// <param name="totalReward">The total fee that will be distributed to the applicable multisig nodes.</param>
         List<Recipient> DistributeToMultisigNodes(uint256 depositId, Money totalReward);
 
         /// <summary>
