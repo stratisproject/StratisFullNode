ebc11498aba0f415c7ad0a349d9ea2f2c0e023c9
Satya Karki <spacegrlsatya@gmail.com>
2022-05-05 14:41:16 +0600

    add TransactionAddedToMemoryPoolClientEvent for SignalR

---
+++ src/Stratis.Bitcoin.Features.SignalR/Events/TransactionAddedToMemoryPoolClientEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stratis.Bitcoin.EventBus;
+using Stratis.Bitcoin.Features.MemoryPool;
+
+namespace Stratis.Bitcoin.Features.SignalR.Events
+{
+    public class TransactionAddedToMemoryPoolClientEvent : IClientEvent
+    {
+        public long MemPoolSize { get; set; }
+        public Type NodeEventType { get; } = typeof(TransactionAddedToMemoryPool);
+        public void BuildFrom(EventBase @event)
+        {
+            if (@event is TransactionAddedToMemoryPool transactionAddedToMemoryPool)
+            {
+                
+                return;
+            }
+
+            throw new ArgumentException();
+        }
+    }
+}
