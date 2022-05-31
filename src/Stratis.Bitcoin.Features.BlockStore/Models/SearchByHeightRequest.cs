4b497d9eade96b7837639b7b14a50e276f91d195
quantumagi <someguy.fromafrica@gmail.com>
2022-05-31 17:43:25 +1000

    Add GetBlocks and CommonBlock APIs for chain synchronisation (#985)
    * Add GetBlocks and CommonBlock APIs for chain synchronisation
    
    * Fix typo
    
    * Change method
    
    * Add tests

---
+++ src/Stratis.Bitcoin.Features.BlockStore/Models/SearchByHeightRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stratis.Bitcoin.Features.BlockStore.Models
+{
+    /// <summary>
+    /// A class containing the necessary parameters for a block search request.
+    /// </summary>
+    public class SearchByHeightRequest : RequestBase
+    {
+        /// <summary>
+        /// The height of the required block(s).
+        /// </summary>
+        [Required]
+        public int Height { get; set; }
+
+        /// <summary>
+        /// The maximum number of the blocks to return.
+        /// </summary>
+        [Required]
+        public int NumberOfBlocks { get; set; }
+
+        /// <summary>
+        /// A flag that indicates whether to return each block transaction complete with details
+        /// or simply return transaction hashes (TX IDs).
+        /// </summary>
+        /// <remarks>This flag is not used when <see cref="RequestBase.OutputJson"/> is set to false.</remarks>
+        public bool ShowTransactionDetails { get; set; }
+    }
+}
