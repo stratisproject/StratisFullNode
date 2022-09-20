e618403887bc46cded673b2fa7fab9b17d130fc8
Satya Karki <spacegrlsatya@gmail.com>
2022-09-20 18:03:17 +0800

    Add a new Peer connection signalr event and sent list of connections in signalr message.

---
+++ src/Stratis.Bitcoin.Features.SignalR/Events/PeerConnectionInfoClientEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stratis.Bitcoin.Connection;
+using Stratis.Bitcoin.EventBus;
+using Stratis.Bitcoin.EventBus.CoreEvents;
+
+namespace Stratis.Bitcoin.Features.SignalR.Events
+{
+    public class PeerConnectionInfoClientEvent : IClientEvent
+    {
+        public IEnumerable<PeerConnectionModel> peerConnectionModels { get; set; }
+        public Type NodeEventType { get; } = typeof(PeerConnectionInfoEvent);
+
+        public void BuildFrom(EventBase @event)
+        {
+            if (@event is PeerConnectionInfoEvent peerConnectionInfo)
+            {
+                this.peerConnectionModels = peerConnectionInfo.peerConnectionModels;
+
+                return;
+            }
+        }
+    }
+}
---
+++ src/Stratis.Bitcoin/EventBus/CoreEvents/PeerConnectionInfoEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stratis.Bitcoin.Connection;
+
+namespace Stratis.Bitcoin.EventBus.CoreEvents
+{
+    public class PeerConnectionInfoEvent : EventBase
+    {
+        public IEnumerable<PeerConnectionModel> peerConnectionModels { get; set; }
+
+        public PeerConnectionInfoEvent(IEnumerable<PeerConnectionModel> peerConnectionModels)
+        {
+            this.peerConnectionModels = peerConnectionModels;
+        }
+    }
+}
