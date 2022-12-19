2b5ee2637a2249efec95d5174786a07dbd23ccf6
Kevin Loubser <zeptin@gmail.com>
2022-12-19 10:52:28 +0200

    Bump CI again

--- src/Stratis.SmartContracts.CLR/SCL/Operations.cs
+++ src/Stratis.SmartContracts.CLR/SCL/Operations.cs
@@ -4,6 +4,9 @@ namespace Stratis.SCL.Base
 {
     public static class Operations
     {
+        /// <summary>
+        /// A no-operation, i.e. it does nothing.
+        /// </summary>
         public static void Noop() { }
     }
 }
