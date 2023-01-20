6afb5fea9d74db2a223bd508d02b60988483b3df
quantumagi <someguy.fromafrica@gmail.com>
2023-01-20 16:31:59 +1100

    Add blacklisting

--- src/Stratis.Bitcoin.Features.Interop/ETHClient/ContractSource/WrappedToken.sol
+++ src/Stratis.Bitcoin.Features.Interop/ETHClient/ContractSource/WrappedToken.sol
@@ -7,6 +7,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contr
 
 contract WrappedToken is ERC20, Ownable {
     mapping (address => string) public withdrawalAddresses;
+    mapping (address => bool) public _isBlacklisted;
     
     constructor(string tokenName, string tokenSymbol, uint256 initialSupply) public ERC20(tokenName, tokenSymbol) {
         _mint(msg.sender, initialSupply);
@@ -58,5 +59,28 @@ contract WrappedToken is ERC20, Ownable {
         // the burner will have to wait until each burn is processed before proceeding with the next.
         string memory key = string(abi.encodePacked(msg.sender, " ", burnId));
         withdrawalAddresses[key] = tokenAddress;
+    }
+
+    function addToBlackList(address[] calldata addresses) public onlyOwner {
+        for (uint256 i; i < addresses.length; ++i) {
+            _isBlacklisted[addresses[i]] = true;
+        }
+    }
+
+    function removeFromBlackList(address account) public onlyOwner {
+        isBlacklisted[account] = false;
+    }
+
+     /**
+     * @dev See {ERC20-_beforeTokenTransfer}.
+     *
+     * Requirements:
+     *
+     * - the contract must not be paused.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        require(!isBlacklisted[from] && !isBlacklisted[to], "This address is blacklisted");
     }
 }
