fc734ae1b81392ad7e7c394174d97a6269d3a855
Kevin Loubser <zeptin@gmail.com>
2023-03-08 20:41:01 +0200

    Add check for empty list

--- src/Stratis.Bitcoin/Utilities/Extensions/ListExtensions.cs
+++ src/Stratis.Bitcoin/Utilities/Extensions/ListExtensions.cs
@@ -9,6 +9,9 @@ namespace Stratis.Bitcoin.Utilities.Extensions
 
         public static void Shuffle<T>(this IList<T> list)
         {
+            if (list.Count == 0) 
+                return;
+
             for (int i = list.Count - 1; i > 1; i--)
             {
                 int rnd = random.Next(i + 1);
