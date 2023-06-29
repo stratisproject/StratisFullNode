07f210299da7609f99df6617c7ae6ac32c1c023c
quantumagi <someguy.fromafrica@gmail.com>
2023-06-29 11:09:03 +1000

    Update batch activation height

--- src/Stratis.Bitcoin.Networks/StraxTest.cs
+++ src/Stratis.Bitcoin.Networks/StraxTest.cs
@@ -35,7 +35,7 @@ namespace Stratis.Bitcoin.Networks
             this.DefaultBanTimeSeconds = 11250; // 500 (MaxReorg) * 45 (TargetSpacing) / 2 = 3 hours, 7 minutes and 30 seconds
 
             this.CirrusRewardDummyAddress = "tGXZrZiU44fx3SQj8tAQ3Zexy2VuELZtoh";
-            this.RewardClaimerBatchActivationHeight = 1;
+            this.RewardClaimerBatchActivationHeight = 500;
             this.RewardClaimerBlockInterval = 100;
 
             this.ConversionTransactionFeeDistributionDummyAddress = "tUAzRBe1CaKaZnrxWPLVv7F4owHHKXAtbj";
