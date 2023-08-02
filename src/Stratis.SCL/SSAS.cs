1779be4ca9adaa35dd38fbe6afbfc60e77f720a1
quantumagi <someguy.fromafrica@gmail.com>
2023-08-02 18:34:27 +1000

    Revert fix for now

--- src/Stratis.SCL/SSAS.cs
+++ src/Stratis.SCL/SSAS.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
 using NBitcoin;
 using NBitcoin.DataEncoders;
 using Nethereum.RLP;
@@ -20,7 +22,7 @@ namespace Stratis.SCL.Crypto
             try
             {
                 // Create a mapping of available url arguments.
-                Dictionary<string, string> argDict = ParseQueryString(url);
+                Dictionary<string, StringValues> argDict = QueryHelpers.ParseQuery(new Uri(url).Query);
 
                 // The "signatureTemplateMap" takes the following form: "uid#11,symbol#4,amount#12,targetAddres#4,targetNetwork#4"
                 var arguments = signatureTemplateMap
@@ -28,7 +30,7 @@ namespace Stratis.SCL.Crypto
                     .Select(argName =>
                     {
                         var argNameSplit = argName.Split("#");
-                        var argValue = argDict[argNameSplit[0]];
+                        var argValue = argDict[argNameSplit[0]].ToString();
                         var fieldType = int.Parse(argNameSplit[1]);
                         var hexEncoder = new HexEncoder();
                         byte[] argumentBytes;
@@ -41,7 +43,6 @@ namespace Stratis.SCL.Crypto
                                 // URL's should contain UInt128 as 16 bytes encoded in hexadecimal.
                                 if (argValue.Length != 32)
                                     return null;
-
                                 argumentBytes = hexEncoder.DecodeData(argValue);
                                 break;
                             case 12:
@@ -50,7 +51,6 @@ namespace Stratis.SCL.Crypto
                                 if (argValue.Contains('.'))
                                 {
                                     decimal amt = decimal.Parse(argValue);
-
                                     // Convert to UInt256.
                                     argumentBytes = ((UInt256)((ulong)(amt * 100000000 /* 8 decimals */))).ToBytes();
                                 }
@@ -59,20 +59,16 @@ namespace Stratis.SCL.Crypto
                                     // URL's should contain UInt256 as 32 bytes encoded in hexadecimal.
                                     if (argValue.Length != 32)
                                         return null;
-
                                     argumentBytes = hexEncoder.DecodeData(argValue);
                                 }
                                 break;
-
                             // TODO: Handle more types as required.
-
                             default:
                                 return null;
                         }
                         return argumentBytes;
                     })
                     .ToArray();
-
                 // Convert the list of objects to RLE.
                 return RLP.EncodeElementsAndList(arguments);
             }
@@ -80,29 +76,6 @@ namespace Stratis.SCL.Crypto
             {
                 return null;
             }
-        }
-
-        private static Dictionary<string, string> ParseQueryString(string queryString)
-        {
-            Dictionary<string, string> result = new Dictionary<string, string>();
-
-            if (!string.IsNullOrEmpty(queryString))
-            {
-                // Remove the '?' at the start of the query string
-                if (queryString[0] == '?') queryString = queryString.Substring(1);
-
-                var queryParts = queryString.Split('&');
-
-                foreach (var part in queryParts)
-                {
-                    var keyValue = part.Split('=');
-
-                    if (keyValue.Length == 2)
-                        result[keyValue[0]] = Uri.UnescapeDataString(keyValue[1]);
-                }
-            }
-
-            return result;
         }
     }
 }
