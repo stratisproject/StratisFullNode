78519405170dbb80743966d951f8b1eb0c752658
quantumagi <someguy.fromafrica@gmail.com>
2023-09-14 19:52:46 +1000

    Stratis.SmartContracts.CLR prerelease version

--- src/Stratis.SmartContracts.CLR/Compilation/ReferencedAssemblyResolver.cs
+++ src/Stratis.SmartContracts.CLR/Compilation/ReferencedAssemblyResolver.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Stratis.SCL;
 using Stratis.SmartContracts.Standards;
 
 namespace Stratis.SmartContracts.CLR.Compilation
